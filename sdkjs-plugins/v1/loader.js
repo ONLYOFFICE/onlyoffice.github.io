349a1e2342f5a6b0fcde8e6063c07422abace17c
Julia Radzhabova <julia.radzhabova@onlyoffice.com>
2020-12-17 13:46:15 +0300

    Fix loader

--- sdkjs-plugins/v1/loader.js
+++ sdkjs-plugins/v1/loader.js
@@ -3,6 +3,7 @@
  * var loader = showLoader(document.getElementById("parent-id"))
  * loader.remove()
  *
+ * parent must have position: relative
  */
 showLoader = function (parent, text) {
     var loader = $('<div class="asc-plugin-loader" ><div class="asc-loader-image"></div><div class="asc-loader-title">' + (text || 'Loading') + '</div></div>');
