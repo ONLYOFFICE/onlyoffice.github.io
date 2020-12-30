119b1705851120228900e4efa92df0950ad95134
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2020-12-30 16:07:26 +0300

    Fix bug #47773

--- scripts/translate.js
+++ scripts/translate.js
@@ -1,9 +1,15 @@
 (function(window, undefined){
 	var isInit = false;
 	var ifr;
+	const isIE = checkInternetExplorer();	//check IE
+	var message = "This plugin doesn't work in Internet Explorer."
 	
 	window.Asc.plugin.init = function(text)
 	{
+		if (isIE) {
+			document.getElementById("iframe_parent").innerHTML = "<h4 id='h4' style='margin:5px'>" + message + "</h4>";
+			return;
+		}
 		if (!isInit) {
 			document.getElementById("iframe_parent").innerHTML = "";
 			ifr                = document.createElement("iframe");
@@ -33,6 +39,25 @@
 			ifr.contentDocument.getElementById("google_translate_element").style.opacity = 0;
 		}
 	};
+
+	function checkInternetExplorer(){
+		var rv = -1;
+		if (window.navigator.appName == 'Microsoft Internet Explorer') {
+			const ua = window.navigator.userAgent;
+			const re = new RegExp('MSIE ([0-9]{1,}[\.0-9]{0,})');
+			if (re.exec(ua) != null) {
+				rv = parseFloat(RegExp.$1);
+			}
+		} else if (window.navigator.appName == 'Netscape') {
+			const ua = window.navigator.userAgent;
+			const re = new RegExp('Trident/.*rv:([0-9]{1,}[\.0-9]{0,})');
+
+			if (re.exec(ua) != null) {
+				rv = parseFloat(RegExp.$1);
+			}
+		}
+		return rv !== -1;
+	};
 	
 	window.Asc.plugin.button = function(id)
 	{
@@ -46,6 +71,16 @@
 			false, false, false, false, 0, null);
 
 		document.dispatchEvent(evt);
+	};
+
+	window.Asc.plugin.onTranslate = function()
+	{
+		if (isIE) {
+			var field = document.getElementById("h4");
+			if (field)
+				field.innerHTML = message = window.Asc.plugin.tr(message);
+		}
+		
 	};
 
 })(window, undefined);
