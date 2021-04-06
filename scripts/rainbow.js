598c321025df27888ff3d185511346dec680afef
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-04-06 15:07:58 +0300

    Fixes

--- scripts/rainbow.js
+++ scripts/rainbow.js
@@ -1,44 +1,58 @@
-/**
- *
- * (c) Copyright Ascensio System SIA 2020
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+(function(window, undefined){
+	var isInit = false;
+	var ifr;
 
-(function(window, undefined) {
-    window.oncontextmenu = function(e)
-    {
-        if (e.preventDefault)
-            e.preventDefault();
-        if (e.stopPropagation)
-            e.stopPropagation();
-        return false;
-    };
+	window.Asc.plugin.init = function(text)
+	{
+		if (isIE) {
+			document.getElementById("iframe_parent").innerHTML = "<h4 id='h4' style='margin:5px'>" + message + "</h4>";
+			return;
+		}
+		if (!isInit) {
+			document.getElementById("iframe_parent").innerHTML = "";
+			ifr                = document.createElement("iframe");
+			ifr.position	   = "fixed";
+			ifr.name           = "google_name";
+			ifr.id             = "google_id";
+			ifr.src            = "https://web.openrainbow.com/";//?text=" + encodeURIComponent(text);
+			ifr.style.top      = "0px";
+			ifr.style.left     = "0px";
+			ifr.style.width    = "100%";
+			ifr.style.height   = "100%";
+			ifr.setAttribute("frameBorder", "0");
+			ift.setAttribute("allow", "camera; microphone");
+			document.getElementById("iframe_parent").appendChild(ifr);
+			isInit = true;
+			ifr.onload = function() {
+				console.log('Downloaded...')
+			}
+		} else {
+			console.log('Error while loading...')
+		}
+	};
 
-    window.Asc.plugin.init = function () {
-    };
+	window.Asc.plugin.button = function(id)
+	{
+		this.executeCommand("close", "");
+	};
 
-    window.Asc.plugin.button = function (id) {
-            this.executeCommand("close", '');
-    };
+	window.Asc.plugin.onExternalMouseUp = function()
+	{
+		var evt = document.createEvent("MouseEvents");
+		evt.initMouseEvent("mouseup", true, true, window, 1, 0, 0, 0, 0,
+			false, false, false, false, 0, null);
 
-    window.Asc.plugin.onExternalMouseUp = function()
-    {
-        var evt = document.createEvent("MouseEvents");
-        evt.initMouseEvent("mouseup", true, true, window, 1, 0, 0, 0, 0,
-            false, false, false, false, 0, null);
+		document.dispatchEvent(evt);
+	};
 
-        document.dispatchEvent(evt);
-    };
-})(window);
+	window.Asc.plugin.onTranslate = function()
+	{
+		if (isIE) {
+			var field = document.getElementById("h4");
+			if (field)
+				field.innerHTML = message = window.Asc.plugin.tr(message);
+		}
+		
+	};
+
+})(window, undefined);
