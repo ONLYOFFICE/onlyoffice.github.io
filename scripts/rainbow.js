de2965be757f80c045df561cbc97aab743b42b28
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-04-06 15:07:58 +0300

    Fixes

--- scripts/rainbow.js
+++ scripts/rainbow.js
@@ -1,58 +1,58 @@
-/*
- * (c) Copyright Ascensio System SIA 2010
- *
- * This program is a free software product. You can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License (AGPL)
- * version 3 as published by the Free Software Foundation. In accordance with
- * Section 7(a) of the GNU AGPL its Section 15 shall be amended to the effect
- * that Ascensio System SIA expressly excludes the warranty of non-infringement
- * of any third-party rights.
- *
- * This program is distributed WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
- * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
- *
- * You can contact Ascensio System SIA at 20A-6 Ernesta Birznieka-Upish
- * street, Riga, Latvia, EU, LV-1050.
- *
- * The  interactive user interfaces in modified source and object code versions
- * of the Program must display Appropriate Legal Notices, as required under
- * Section 5 of the GNU AGPL version 3.
- *
- * Pursuant to Section 7(b) of the License you must retain the original Product
- * logo when distributing the program. Pursuant to Section 7(e) we decline to
- * grant you any rights under trademark law for use of our trademarks.
- *
- * All the Product's GUI elements, including illustrations and icon sets, as
- * well as technical writing content are licensed under the terms of the
- * Creative Commons Attribution-ShareAlike 4.0 International. See the License
- * terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
- *
- */
+(function(window, undefined){
+	var isInit = false;
+	var ifr;
 
-(function(window, undefined) {
-    window.oncontextmenu = function(e)
-    {
-        if (e.preventDefault)
-            e.preventDefault();
-        if (e.stopPropagation)
-            e.stopPropagation();
-        return false;
-    };
+	window.Asc.plugin.init = function(text)
+	{
+		if (isIE) {
+			document.getElementById("iframe_parent").innerHTML = "<h4 id='h4' style='margin:5px'>" + message + "</h4>";
+			return;
+		}
+		if (!isInit) {
+			document.getElementById("iframe_parent").innerHTML = "";
+			ifr                = document.createElement("iframe");
+			ifr.position	   = "fixed";
+			ifr.name           = "google_name";
+			ifr.id             = "google_id";
+			ifr.src            = "https://web.openrainbow.com/";//?text=" + encodeURIComponent(text);
+			ifr.style.top      = "0px";
+			ifr.style.left     = "0px";
+			ifr.style.width    = "100%";
+			ifr.style.height   = "100%";
+			ifr.setAttribute("frameBorder", "0");
+			ift.setAttribute("allow", "camera; microphone");
+			document.getElementById("iframe_parent").appendChild(ifr);
+			isInit = true;
+			ifr.onload = function() {
+				console.log('Downloaded...')
+			}
+		} else {
+			console.log('Error while loading...')
+		}
+	};
 
-    window.Asc.plugin.init = function () {
-    };
+	window.Asc.plugin.button = function(id)
+	{
+		this.executeCommand("close", "");
+	};
 
-    window.Asc.plugin.button = function (id) {
-            this.executeCommand("close", '');
-    };
+	window.Asc.plugin.onExternalMouseUp = function()
+	{
+		var evt = document.createEvent("MouseEvents");
+		evt.initMouseEvent("mouseup", true, true, window, 1, 0, 0, 0, 0,
+			false, false, false, false, 0, null);
 
-    window.Asc.plugin.onExternalMouseUp = function()
-    {
-        var evt = document.createEvent("MouseEvents");
-        evt.initMouseEvent("mouseup", true, true, window, 1, 0, 0, 0, 0,
-            false, false, false, false, 0, null);
+		document.dispatchEvent(evt);
+	};
 
-        document.dispatchEvent(evt);
-    };
-})(window);
+	window.Asc.plugin.onTranslate = function()
+	{
+		if (isIE) {
+			var field = document.getElementById("h4");
+			if (field)
+				field.innerHTML = message = window.Asc.plugin.tr(message);
+		}
+		
+	};
+
+})(window, undefined);
