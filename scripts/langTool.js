c3c6ea97fa77013d582df6844cc5170d1a29e976
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-04-09 13:51:48 +0300

    Added support for changing theme

--- scripts/langTool.js
+++ scripts/langTool.js
@@ -161,7 +161,7 @@
 				return el;
 			});
 			parseResult(oResponse);
-			console.log(oResponse);
+			$('.result_div').css('background', window.Asc.plugin.theme["background-normal"]);
 			showLoader(elements, false);
 		}).error(function(e){
 		    $('<span>', {
