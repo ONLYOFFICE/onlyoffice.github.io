aa25c5a7b5e0e1107da547ff4257b798b262b7b3
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-04-23 15:33:28 +0300

    Added insert text if text no selected

--- scripts/deepL.js
+++ scripts/deepL.js
@@ -283,8 +283,18 @@ var Ps;
                         window.Asc.plugin.executeMethod("PasteText", [$("#txt_shower")[0].innerText]);
                     }
                     else {
-                        window.Asc.plugin.callCommand(function() {
-                            Api.ReplaceTextSmart(Asc.scope.arr);
+                        window.Asc.plugin.executeMethod("GetSelectionType", [], function(sType) {
+                            switch (sType) {
+                                case "none":
+                                case "drawing":
+                                    window.Asc.plugin.executeMethod("PasteText", [$("#txt_shower")[0].innerText]);
+                                    break;
+                                case "text":
+                                    window.Asc.plugin.callCommand(function() {
+                                        Api.ReplaceTextSmart(Asc.scope.arr);
+                                    });
+                                    break;
+                            }
                         });
                     }
                 });
