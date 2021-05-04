3aa3bad04452c34316de564829bacbe86a8df498
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2021-05-04 15:39:03 +0300

    Fix bug #50178

--- scripts/code.js
+++ scripts/code.js
@@ -110,6 +110,8 @@
 		window.Asc.plugin.onThemeChangedBase(theme);
 		if (theme.type.indexOf("dark") !== -1)
 			setTimeout(()=>editor.setOption("theme", "bespin"));
+		else
+			setTimeout(()=>editor.setOption("theme", "default"));
 	};
 
 })(window, undefined);
