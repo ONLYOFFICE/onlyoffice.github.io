57233fefaf394d949f673edda81a002a69c6cba2
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-05-04 18:58:53 +0300

    Fix background-color in list after 23 macros

--- scripts/code.js
+++ scripts/code.js
@@ -470,7 +470,8 @@ ace.config.loadModule('ace/ext/html_beautify', function (beautify) {
         $('#menu, .divFooter').css('border-color', window.Asc.plugin.theme["border-divider"]);
         $('.ace_scroller').css('border-left', 'solid 1px ' + window.Asc.plugin.theme["border-divider"]);
         $('.ace_cursor').css('color', window.Asc.plugin.theme["text-normal"]);
-
+        $('#menu').css('background-color', window.Asc.plugin.theme["background-normal"]);
+        
         var rules = '.macros { color: ' + window.Asc.plugin.theme["text-normal"] + '; background-color: ' + window.Asc.plugin.theme['background-toolbar'] + '}\n';
         rules += '.macros:hover { background-color: ' + window.Asc.plugin.theme['highlight-button-hover'] + '}\n';
         rules += '.macrosSelected { background-color: ' + window.Asc.plugin.theme['highlight-button-pressed'] + '}\n';
