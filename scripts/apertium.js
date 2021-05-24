08faf834cd2538be22e23b9486be64d91957c6d4
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-05-24 11:44:15 +0300

    Fix dynamic text area size

--- scripts/apertium.js
+++ scripts/apertium.js
@@ -398,7 +398,7 @@ var Ps;
             var el = document.getElementById('textarea');
             setTimeout(function() {
                 el.style.cssText = 'height:100px  !important; width: 100%;';
-                el.style.cssText = 'height:' + Math.max(98, el.scrollHeight + 2) + 'px !important; width:100%;';
+                el.style.cssText = 'height:' + Math.max(98, Math.min(el.scrollHeight + 2, nBodyHeight/2)) + 'px !important; width:100%;';
             }, 1);
         };
         functionResize = resize;
