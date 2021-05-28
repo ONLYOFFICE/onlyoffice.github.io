576f019353712d34e7171f5d3c36efc5345b05e6
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-05-28 17:49:12 +0300

    Changed input text area to div text area

--- scripts/deepL.js
+++ scripts/deepL.js
@@ -30,6 +30,7 @@
  *
  */
 var Ps;
+var PsTextArea;
 (function(window, undefined){
 
     var txt              = "";
@@ -40,7 +41,6 @@ var Ps;
     var apikey           = "";
     var isValidKey       = false;
     var isFirstRun       = true;
-    var functionResize;
 	function showLoader(elements, show) {
 
        switchClass(elements.contentHolder, blurClass, show);
@@ -62,8 +62,7 @@ var Ps;
 		});
 
         txt = text;
-        document.getElementById("textarea").value = text;
-        functionResize();
+        document.getElementById("textarea").innerText = text;
 
         if ((apikey == '' || apikey == null) && isFirstRun) {
             isFirstRun = false;
@@ -240,6 +239,7 @@ var Ps;
 		};
 
         Ps = new PerfectScrollbar("#display", {suppressScrollX: true});
+        PsTextArea = new PerfectScrollbar("#enter_container", { suppressScrollX  : true});
 
         setTimeout(function() {
             document.getElementById("copy").onclick = function () {
@@ -335,15 +335,15 @@ var Ps;
 
         $('#show_manually').click(function() {
             $(this).hide();
-            functionResize();
             $('#hide_manually').show();
             $('#enter_container').show();
+            updateScroll();
         });
         $('#hide_manually').click(function() {
             $(this).hide();
-            functionResize();
             $('#show_manually').show();
             $('#enter_container').hide();
+            updateScroll();
         });
 
         function delay(callback, ms) {
@@ -358,8 +358,7 @@ var Ps;
         };
 
         $('#textarea').keyup(delay(function(e) {
-            document.getElementById("textarea").value = document.getElementById("textarea").value;
-            txt = document.getElementById("textarea").value;
+            txt = document.getElementById("textarea").innerText;
             switch (window.Asc.plugin.info.editorType) {
                 case 'word':
                 case 'slide': {
@@ -374,25 +373,12 @@ var Ps;
                 break;
             }
         }, 500));
-        var textarea = document.getElementsByTagName('textarea')[0];
-        textarea.addEventListener('keydown', resize);
-        function resize() {
-            var nBodyHeight = document.querySelector('body').offsetHeight;
-            var nTextAreaHeight = document.querySelector('textarea').offsetHeight;
-
-            var el = document.getElementById('textarea');
-            setTimeout(function() {
-                el.style.cssText = 'height:100px  !important; width: 100%;';
-                el.style.cssText = 'height:' + Math.max(98, Math.min(el.scrollHeight + 2, nBodyHeight/2)) + 'px !important; width:100%;';
-                updateScroll();
-            }, 1);
-        };
-        functionResize = resize;
     })
 
     function updateScroll()
 	{
 		Ps && Ps.update();
+		PsTextArea && PsTextArea.update();
 	};
 
 	window.Asc.plugin.button = function(id)
