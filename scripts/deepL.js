b2a43116cd88526b9351ae71727319576d27d99f
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-06-01 18:58:56 +0300

    Fix for select2

--- scripts/deepL.js
+++ scripts/deepL.js
@@ -74,7 +74,11 @@ var PsTextArea;
     window.Asc.plugin.onThemeChanged = function(theme)
     {
         window.Asc.plugin.onThemeChangedBase(theme);
-
+        var rule = ".select2-container--default.select2-container--open .select2-selection__arrow b { border-color : " + window.Asc.plugin.theme["text-normal"] + " !important; }";
+        var styleTheme = document.createElement('style');
+        styleTheme.type = 'text/css';
+        styleTheme.innerHTML = rule;
+        document.getElementsByTagName('head')[0].appendChild(styleTheme);
         $('#show_manually, #hide_manually, #reconf').css('border-bottom', '1px dashed ' + window.Asc.plugin.theme.Color);
     };
 
