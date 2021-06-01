1c072fe4c398ce0f8b22b605af339c68a63b011a
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-06-01 19:06:24 +0300

    Fix for select2

--- scripts/zoom.js
+++ scripts/zoom.js
@@ -91,6 +91,13 @@ var Ps;
     window.Asc.plugin.onThemeChanged = function(theme)
     {
         window.Asc.plugin.onThemeChangedBase(theme);
+
+        var rule = ".select2-container--default.select2-container--open .select2-selection__arrow b { border-color : " + window.Asc.plugin.theme["text-normal"] + " !important; }";
+        var styleTheme = document.createElement('style');
+        styleTheme.type = 'text/css';
+        styleTheme.innerHTML = rule;
+        document.getElementsByTagName('head')[0].appendChild(styleTheme);
+
         $('#reconf-label, #switch-label, #label-settings').css('border-bottom', '1px dashed ' + window.Asc.plugin.theme.Color + ' !important');
         $('.arrow').css('border-color', window.Asc.plugin.theme["text-normal"]);
     };
