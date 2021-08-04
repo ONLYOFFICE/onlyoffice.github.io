e865e7b2c45a96a4d2c04af60515daea613dfc33
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-08-04 16:58:38 +0300

    Fix bug with text inserting

--- scripts/apertium.js
+++ scripts/apertium.js
@@ -407,7 +407,7 @@ var PsTextArea;
 
                     window.Asc.plugin.executeMethod("GetVersion", [], function(version) {
                         if (version === undefined) {
-                            window.Asc.plugin.executeMethod("PasteText", [$("#txt_shower")[0].innerText]);
+                           paste_done = window.Asc.plugin.executeMethod("PasteText", [$("#txt_shower")[0].innerText]);
                         }
                         else {
                             window.Asc.plugin.executeMethod("GetSelectionType", [], function(sType) {
