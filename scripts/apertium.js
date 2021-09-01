61dcb87f770f44eaaa13bfbb9021970e96ac690e
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-09-01 18:38:58 +0300

    Fixed argument in method.

--- scripts/apertium.js
+++ scripts/apertium.js
@@ -86,7 +86,7 @@ function getMessage(key) {
         switch (window.Asc.plugin.info.editorType) {
             case 'word': {
                 if (txt !== "") {
-                    window.Asc.plugin.executeMethod("GetSelectedText", [false], function(data) {
+                    window.Asc.plugin.executeMethod("GetSelectedText", [{Numbering:false}], function(data) {
                         txt = data;
                         ExecPlugin();
                     });
