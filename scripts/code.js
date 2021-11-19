4054098b3b7d64bdb024ffeb6ec9eceb31c7467d
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2021-11-19 11:40:33 +0300

    Added source files

---
+++ scripts/code.js
@@ -0,0 +1,80 @@
+/*
+ * (c) Copyright Ascensio System SIA 2010
+ *
+ * This program is a free software product. You can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License (AGPL)
+ * version 3 as published by the Free Software Foundation. In accordance with
+ * Section 7(a) of the GNU AGPL its Section 15 shall be amended to the effect
+ * that Ascensio System SIA expressly excludes the warranty of non-infringement
+ * of any third-party rights.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
+ * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+ *
+ * You can contact Ascensio System SIA at 20A-6 Ernesta Birznieka-Upish
+ * street, Riga, Latvia, EU, LV-1050.
+ *
+ * The  interactive user interfaces in modified source and object code versions
+ * of the Program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU AGPL version 3.
+ *
+ * Pursuant to Section 7(b) of the License you must retain the original Product
+ * logo when distributing the program. Pursuant to Section 7(e) we decline to
+ * grant you any rights under trademark law for use of our trademarks.
+ *
+ * All the Product's GUI elements, including illustrations and icon sets, as
+ * well as technical writing content are licensed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International. See the License
+ * terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ *
+ */
+(function(window, undefined){
+
+	var iframe,
+		isInit = false,
+		api;
+	
+	window.Asc.plugin.init = function () {
+		//event "init" for plugin
+		document.getElementById("start").onclick = function() {
+			if (!isInit) {
+				//create iframe jitsi
+				const domain = 'meet.jit.si';
+				const options = {
+					roomName: 'Test meet',
+					width: document.getElementById("body").clientWidth- 10 +"px",
+					height: '550px',
+					parentNode: document.querySelector('#meet'),
+					interfaceConfigOverwrite: { SHOW_CHROME_EXTENSION_BANNER: false },
+					onload : function () {
+						if (isInit) {
+							api.dispose();
+							isInit = false;
+						} else {
+							isInit = true;
+						}
+					}
+				};
+				api = new JitsiMeetExternalAPI(domain, options);
+				iframe = api.getIFrame();
+			}
+		};
+
+		document.getElementById("stop").onclick = function() {
+			//destroy iframe jisti
+			api.dispose();
+			isInit = false;
+		};
+	};
+
+	window.onresize = function(e){
+		//event resize for window
+		iframe.style.width = document.getElementById("body").clientWidth- 10 +"px";
+	}
+	window.Asc.plugin.button = function() {		
+		this.executeCommand("close", "");
+	};
+
+
+})(window, undefined);
