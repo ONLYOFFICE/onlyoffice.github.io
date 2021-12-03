1d197e976b071bb58dd81cce8e1c927c1b22efa4
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2021-12-03 12:29:26 +0300

    The source files have been added

---
+++ README.md
@@ -0,0 +1,19 @@
+## Overview
+
+Create, edit and paste diagrams into document. 
+
+This plugin uses [draw.io](https://github.com/jgraph/drawio), this project, is a configurable diagramming/whiteboarding visualization application. draw.io is owned and developed by JGraph Ltd, a UK based software company.
+
+The plugin is compatible with [self-hosted](https://github.com/ONLYOFFICE/DocumentServer) and [desktop](https://github.com/ONLYOFFICE/DesktopEditors) versions of ONLYOFFICE editors. It can be added to ONLYOFFICE instances manually. 
+
+## How to use
+
+1. Open the Plugins tab and press "draw.io".
+2. Create your own diagram or paste it from the available resources.
+3. Click "Save & exit" or "Save" and then "Exit".
+4. You can see the comleted diagram, if you want to edit it, just click on it.
+5. If you want to inser the comleted diagram into the document click button "Ok". 
+
+## How to install
+
+Detailed instructions can be found in [ONLYOFFICE API documentation](https://api.onlyoffice.com/plugin/installation).
---
+++ scripts/code.js
@@ -0,0 +1,123 @@
+/*
+ * (c) Copyright Ascensio System SIA 2010
+ *
+ * This program is a free software product. You can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License (AGPL)
+ * version 3 as published by the Free Software Foundation. In accordance with
+ * Section 7(a) of the GNU AGPL its Section 15 shall be amended to the effect
+ * that Ascensio System SIA expressly excludes the warranty of non-infringement
+ * of any third-party rights.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
+ * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+ *
+ * You can contact Ascensio System SIA at 20A-6 Ernesta Birznieka-Upish
+ * street, Riga, Latvia, EU, LV-1050.
+ *
+ * The  interactive user interfaces in modified source and object code versions
+ * of the Program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU AGPL version 3.
+ *
+ * Pursuant to Section 7(b) of the License you must retain the original Product
+ * logo when distributing the program. Pursuant to Section 7(e) we decline to
+ * grant you any rights under trademark law for use of our trademarks.
+ *
+ * All the Product's GUI elements, including illustrations and icon sets, as
+ * well as technical writing content are licensed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International. See the License
+ * terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ *
+ */
+(function(window, undefined) {
+
+	var img;
+	var editor;
+	var UI = "kennedy";
+	var loader;
+	var lang = "";
+
+	window.Asc.plugin.init = function(data)
+	{
+		lang = this.info.lang.split('-')[0];
+		img = document.createElement('img');
+		document.getElementById("div_preview").appendChild(img);
+		// create an empti image for editor (if data doesn't set), after export we put the result in it
+		if (!data)
+			data = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY/j//z8ABf4C/qc1gYQAAAAASUVORK5CYII=";
+
+		img.setAttribute('src', data);
+		img.onclick = function() {
+			editor = DiagramEditor.editElement([this, document.getElementById("div_preview")], null, UI, null, ['lang=' + lang], closePlugin, loader);
+			editor.isChanged = true;
+		}
+		editor = DiagramEditor.editElement([img, document.getElementById("div_preview")], null, UI, null, ['lang=' + lang], closePlugin, loader);
+		window.Asc.plugin.resizeWindow(1200, 1000, 800, 600, 0, 0);
+	};
+
+	window.Asc.plugin.button = function(id)
+	{
+		if (id == 0) {
+			if (editor.frame) {
+				// if editor is opened
+				editor.closePlugin();
+			} else {
+				closePlugin(editor.isChanged);
+			}			
+		} else {
+			this.executeCommand("close", "");
+		}
+	};
+
+	function closePlugin (bPaste) {
+		if (bPaste)
+		{
+			var _info = window.Asc.plugin.info;
+			var _method = (_info.objectId === undefined) ? "AddOleObject" : "EditOleObject";
+				
+			var _param = {
+				guid : _info.guid,
+				widthPix : (_info.mmToPx * _info.width) >> 0,
+				heightPix : (_info.mmToPx * _info.height) >> 0,
+				width : _info.width ? _info.width : 100,
+				height : _info.height ? _info.height : 70,
+				imgSrc : img.getAttribute('src'),
+				data : img.getAttribute('src'),
+				objectId : _info.objectId,
+				resize : _info.resize
+			};
+
+			window.Asc.plugin.executeMethod(_method, [_param], function() {
+				window.Asc.plugin.executeCommand("close", "");
+			});
+		}
+
+		window.Asc.plugin.executeCommand("close", "");
+	};
+
+	function makeLoader() {
+		loader && (loader.remove ? loader.remove() : $('#loader-container')[0].removeChild(loader));
+		loader = showLoader($('#loader-container')[0], window.Asc.plugin.tr('Loading') + '...');
+	};
+
+	window.Asc.plugin.onTranslate = function()
+	{
+		makeLoader();
+		//nothing to translate
+	};
+
+	window.Asc.plugin.onExternalMouseUp = function()
+	{
+		var evt = document.createEvent("MouseEvents");
+		evt.initMouseEvent("mouseup", true, true, window, 1, 0, 0, 0, 0,
+			false, false, false, false, 0, null);
+		document.dispatchEvent(evt);
+	};
+
+	window.Asc.plugin.onThemeChanged = function(theme)
+	{
+		window.Asc.plugin.onThemeChangedBase(theme);
+		UI = (theme.type.indexOf("dark") !== -1) ? "dark" : "kennedy";
+	};
+
+})(window, undefined);
