674a0e7f903082035aae1b6cf5bbefb0096308fe
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2022-03-23 17:21:57 +0300

    Disabled input box and button in viewer mode

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -20,7 +20,6 @@
 	var url = "";
 	var player = null;
 	var isWindowPlayer = false;
-	var isViewerMode = false;
 
 	function getParam(url, param)
 	{
@@ -75,6 +74,13 @@
 	window.Asc.plugin.init = function(text)
 	{
 	    var _textbox = document.getElementById("textbox_url");
+
+		// disable input and button elements in view mode
+		if (this.info.isViewMode) {
+			_textbox.disabled = true;
+			document.getElementById("textbox_button").disabled = true;
+		}
+
 	    _textbox.onkeyup = function(e)
 	    {
 	        if (e.keyCode == 13) // click on Enter
