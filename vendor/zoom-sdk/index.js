9d3cb8448071f9a3a7a50408e06666bc8d3cdd95
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2022-04-28 16:48:15 +0300

    Fixed reading data from localStorage

--- vendor/zoom-sdk/index.js
+++ vendor/zoom-sdk/index.js
@@ -28,15 +28,14 @@ function websdkready() {
   // ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/@zoomus/websdk/dist/lib', '/av'); // Local version default, Angular Project change to use cdn version
   ZoomMtg.preLoadWasm(); // pre download wasm file to save time.
 
-  var API_KEY = localStorage.getItem('zoom-client-key') || "";
+  var API_KEY, API_SECRET;
 
   /**
    * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
    * The below generateSignature should be done server side as not to expose your api secret in public
    * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/web/essential/signature
    */
-  var API_SECRET = localStorage.getItem('zoom-secret-key') || "";
-
+  
   // some help code, remember mn, pwd, lang to cookie, and autofill.
   document.getElementById("display_name").value = localStorage.getItem($('#display_name').attr('data-id')) || "";
   document.getElementById("meeting_number").value = testTool.getCookie(
@@ -83,7 +82,9 @@ function websdkready() {
     });
 
   // click join meeting button
-  window.joinMeeting = function (element) {
+  window.joinMeeting = function () {
+      API_KEY = localStorage.getItem('zoom-client-key') || "";
+      API_SECRET = API_SECRET = localStorage.getItem('zoom-secret-key') || "";
 
       var meetingConfig = testTool.getMeetingConfig();
       if (!meetingConfig.mn || !meetingConfig.name) {
