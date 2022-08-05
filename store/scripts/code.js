5fed59c7a5d41e70e518fada7442fbc5fea541ac
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2022-08-05 18:03:37 +0300

    .

--- store/scripts/code.js
+++ store/scripts/code.js
@@ -835,8 +835,8 @@ function changeIcons() {
 		guid = arr[i].getAttribute('data-guid');
 		arr[i].src = getImageUrl(guid);
 	}
-	guid = elements.imgIcon.parentNode.parentNode.getAttribute('data-guid');
-	elements.imgIcon.setAttribute(src, getImageUrl());
+	// guid = elements.imgIcon.parentNode.parentNode.getAttribute('data-guid');
+	// elements.imgIcon.setAttribute(src, getImageUrl());
 };
 
 function getTranslation() {
@@ -939,7 +939,7 @@ function getImageUrl(guid, bNotForStore) {
 			iconScale = '/icon@2x.png'
 			break;
 	}
-	let curIcon = './resources/img/defaults/' + themeType + iconScale;
+	let curIcon = './resources/img/defaults/' + themeType + '/icon@2x.png'; // временно сделано так, потому что не пока нет подходящих дефолтных иконок (используются от плагинов)
 
 	let plugin = allPlugins.find(function(el){
 		return el.guid === guid
