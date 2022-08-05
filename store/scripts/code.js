b28f58d6a0a87b44a6510a1f5ccff9dfe2a7fcde
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2022-08-05 20:42:10 +0300

    .

--- store/scripts/code.js
+++ store/scripts/code.js
@@ -750,7 +750,6 @@ window.onresize = function() {
 };
 
 function changeIcons() {
-	console.log('changeIcons');
 	let arr = document.getElementsByClassName('plugin_icon');
 	for (let i = 0; i < arr.length; i++) {
 		arr[i].setAttribute('src', getImageUrl( arr[i].getAttribute('data-guid') ) );
