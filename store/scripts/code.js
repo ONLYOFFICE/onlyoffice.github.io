179cef4c96239d1157fcffb4efb8babdc1a4f289
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2022-08-09 20:45:07 +0300

    .

--- store/scripts/code.js
+++ store/scripts/code.js
@@ -201,19 +201,18 @@ window.addEventListener('message', function(message) {
 			}
 			plugin = allPlugins.find(function(el) {return el.guid === message.guid});
 			installed = installedPlugins.find(function(el){return el.guid === message.guid});
-			// TODO поправить
-			// Сделал пока для проверки, чтобы не удалять утановленный плагин (надо потом добавить проверку на то, какой у него baseUrl)
-			// если baseUrl локальный, то значит не надо его удалять из списка установленный, чтобы потом можно было установить именно его, а не версию из стора
+			let bUpdate = false;
 			if (installed) {
 				if (plugin && installed.obj.baseUrl.includes('onlyoffice.github.io')) {
 					installedPlugins = installedPlugins.filter(function(el){return el.guid !== message.guid});
+					bUpdate = true;
 				} else {
 					installed.removed = true;
 				}
 			}
 
 			if (elements.btnMyPlugins.classList.contains('btn_toolbar_active')) {
-				if (plugin) {
+				if (bUpdate) {
 					showListofPlugins(false);
 				} else {
 					let btn = this.document.getElementById(message.guid).lastChild.lastChild;
