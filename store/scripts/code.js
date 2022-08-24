4cfe9c2b68b460a5a5d79135e1e6a2d34586357e
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2022-08-24 15:30:20 +0300

    .

--- store/scripts/code.js
+++ store/scripts/code.js
@@ -774,7 +774,7 @@ function setDivHeight() {
 		let height = elements.divScreen.parentNode.clientHeight - elements.divScreen.previousElementSibling.clientHeight - 40 + "px";
 		elements.divScreen.style.height = height;
 		elements.divScreen.style.maxHeight = height;
-		elements.imgScreenshot.maxHeight = height;
+		elements.imgScreenshot.style.maxHeight = height;
 	}
 };
 
