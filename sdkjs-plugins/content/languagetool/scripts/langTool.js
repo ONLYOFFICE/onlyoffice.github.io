3ac283d5c5ba50664e0550485e29d85914f49a8d
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2022-09-02 11:16:35 +0300

    [languageTool] - Changed the appearance of the loader element and other small appearances changes.

--- sdkjs-plugins/content/languagetool/scripts/langTool.js
+++ sdkjs-plugins/content/languagetool/scripts/langTool.js
@@ -146,25 +146,18 @@ function checkInternetExplorer(){
     {
         window.Asc.plugin.onThemeChangedBase(theme);
         var rule = '.arrow { border-color : ' + window.Asc.plugin.theme["text-normal"] + ';}\n'
-        if (theme.type == 'dark') {
-            rule += '.asc-plugin-loader .asc-loader-image { background-image : url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyOCAyOCI+PGNpcmNsZSBjeD0iMTQiIGN5PSIxNCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjZmZmIiBzdHJva2Utd2lkdGg9IjEuNSIgcj0iMTAuMjUiIHN0cm9rZS1kYXNoYXJyYXk9IjE2MCUsIDQwJSIgLz48L3N2Zz4=) !important;}\n';
-        }
-        else if (theme.type == 'light') {
-            rule += '.asc-plugin-loader .asc-loader-image { background-image : url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyMCAyMCI+PGNpcmNsZSBjeD0iMTAiIGN5PSIxMCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjNDQ0IiBzdHJva2Utd2lkdGg9IjEuNSIgcj0iNy4yNSIgc3Ryb2tlLWRhc2hhcnJheT0iMTYwJSwgNDAlIiAvPjwvc3ZnPg==) !important;}\n';
-        }
+		rule += '.asc-plugin-loader .asc-loader-image { background-image : url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyOCAyOCI+PGNpcmNsZSBjeD0iMTQiIGN5PSIxNCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjZmZmIiBzdHJva2Utd2lkdGg9IjEuNSIgcj0iMTAuMjUiIHN0cm9rZS1kYXNoYXJyYXk9IjE2MCUsIDQwJSIgLz48L3N2Zz4=) !important;}\n';
         rule += ".select2-container--default.select2-container--open .select2-selection__arrow b { border-color : " + window.Asc.plugin.theme["text-normal"] + " !important; }";
 
         var styleTheme = document.createElement('style');
         styleTheme.type = 'text/css';
         styleTheme.innerHTML = rule;
         document.getElementsByTagName('head')[0].appendChild(styleTheme);
-        $('.asc-loader-title').css('color', window.Asc.plugin.theme["text-normal"]);
         $('.result_div').css('background', window.Asc.plugin.theme["background-normal"]);
 
         if (!isIE) {
             $('#clear').css('border-bottom', 'var(--scaled-one-pixel, 1px) dotted ' + window.Asc.plugin.theme["text-normal"]);
             $('#enter_container').css('background-color', window.Asc.plugin.theme["background-normal"]);
-            $('.asc-loader-title').css('color', window.Asc.plugin.theme["text-normal"]);
             $('#show_manually, #hide_manually').css('border-bottom', '1px dashed ' + window.Asc.plugin.theme["text-normal"]);
             $('#arrow-svg-path').css('fill', theme["text-normal"]);
         }
@@ -510,15 +503,6 @@ function checkInternetExplorer(){
 			    "class": "dismiss btn-text-default i18n"
 			}).data({ index : ind }).appendTo(dismiss_buttons);
 
-//			$('<button>', {
-//			    text: "Dismiss all",
-//			    click: function () {
-//					$('.dismiss').each(function() {
-//					     $(this).trigger("click");
-//					});
-//				},
-//			    "class": "dismiss_all btn-text-default"
-//			}).appendTo(dismiss_buttons);
 			dismiss_buttons.appendTo(div_details);
 		});
 
@@ -634,8 +618,6 @@ function checkInternetExplorer(){
 
 		var container = document.getElementById('scrollable-container-id');			
         Ps = new PerfectScrollbar('#' + container.id, { minScrollbarLength: 20 });
-			// updateScroll();
-			// updateScroll();
 	};
 	
 	window.Asc.plugin.button = function(id)
