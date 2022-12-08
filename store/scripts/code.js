ce375617269c408acbc4c3d737d1b196c8f6c63d
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2022-12-08 19:13:18 +0300

    .

--- store/scripts/code.js
+++ store/scripts/code.js
@@ -190,13 +190,15 @@ window.onload = function() {
 		{id: 2, text:'Instalations'}
 	];
 	$('#select_categories').select2({
-		minimumResultsForSearch: Infinity
+		minimumResultsForSearch: Infinity,
+		with: "100%"
 	}).on('change', function(e) {
 		console.log(e);
 	});
 
 	$('#select_sortBy').select2({
-		minimumResultsForSearch: Infinity
+		minimumResultsForSearch: Infinity,
+		with: "100%"
 	}).on('change', function(e) {
 		console.log(e);
 	});
