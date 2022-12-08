49b1409709722a7dfb04ce85cf4b891a5431e8a3
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2022-12-08 15:18:25 +0300

    .

--- store/scripts/code.js
+++ store/scripts/code.js
@@ -31,7 +31,7 @@
  */
 
 let start = Date.now();
-let current = {index: 0, screenshots: []};
+let current = {index: 0, screenshots: [], url: ''};
 let allPlugins;                                                      // list of all plugins from config
 let installedPlugins;                                                // list of intalled plugins
 const configUrl = './config.json';                                   // url to config.json
@@ -145,7 +145,7 @@ window.onload = function() {
 		console.log('onclick prev');
 		if (current.index > 0) {
 			current.index--;
-			let url = current.screenshots[current.index];
+			let url = current.url + current.screenshots[current.index];
 			elements.imgScreenshot.setAttribute('src', url);
 			elements.imgScreenshot.onload = function() {
 				elements.imgScreenshot.classList.remove('hidden');
@@ -163,7 +163,7 @@ window.onload = function() {
 		console.log('onclick next');
 		if (current.index < current.screenshots.length - 1) {
 			current.index++;
-			let url = current.screenshots[current.index];
+			let url = current.url + current.screenshots[current.index];
 			elements.imgScreenshot.setAttribute('src', url);
 			elements.imgScreenshot.onload = function() {
 				elements.imgScreenshot.classList.remove('hidden');
@@ -700,7 +700,8 @@ function onClickItem() {
 	if (bCorrectUrl && plugin.variations[0].store && plugin.variations[0].store.screenshots && plugin.variations[0].store.screenshots.length) {
 		current.screenshots = plugin.variations[0].store.screenshots;
 		current.index = 0;
-		let url = plugin.baseUrl + plugin.variations[0].store.screenshots[current.index];
+		current.url = plugin.baseUrl;
+		let url = current.url + current.screenshots[current.index];
 		elements.imgScreenshot.setAttribute('src', url);
 		elements.imgScreenshot.onload = function() {
 			elements.imgScreenshot.classList.remove('hidden');
@@ -789,6 +790,7 @@ function onClickBack() {
 	elements.divArrow.classList.add('hidden');
 	current.index = 0;
 	current.screenshots = [];
+	current.url = '';
 	// elements.arrow.classList.add('hidden');
 	if(Ps) Ps.update();
 };
