6d8af621c9680cc7b6ee9a3585edc1955fba0058
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2023-02-17 18:10:29 +0300

    Fix bug #61168

--- sdkjs-plugins/content/openai/scripts/code.js
+++ sdkjs-plugins/content/openai/scripts/code.js
@@ -114,6 +114,12 @@
 
 		};
 
+		elements.textArea.onkeydown = function(e) {
+			if ( (e.ctrlKey || e.metaKey) && e.key === 'Enter') {
+				elements.btnSubmit.click();
+			}
+		};
+
 		elements.divTokens.onmouseenter = function() {
 			elements.modal.classList.remove('hidden');
 			if (modalTimeout) {
