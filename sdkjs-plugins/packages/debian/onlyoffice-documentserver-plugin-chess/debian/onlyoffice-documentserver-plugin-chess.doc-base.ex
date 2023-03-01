75d8c77f0a154265c509e6c4b2abeffcbcb021a1
Elbakyan Shirak <shirak.elbakyan@onlyoffice.com>
2023-03-01 18:32:51 +0400

    onlyoffice-documentserver-plugin-chess

---
+++ sdkjs-plugins/packages/debian/onlyoffice-documentserver-plugin-chess/debian/onlyoffice-documentserver-plugin-chess.doc-base.ex
@@ -0,0 +1,20 @@
+Document: onlyoffice-documentserver-plugin-chess
+Title: Debian onlyoffice-documentserver-plugin-chess Manual
+Author: <insert document author here>
+Abstract: This manual describes what onlyoffice-documentserver-plugin-chess is
+ and how it can be used to
+ manage online manuals on Debian systems.
+Section: unknown
+
+Format: debiandoc-sgml
+Files: /usr/share/doc/onlyoffice-documentserver-plugin-chess/onlyoffice-documentserver-plugin-chess.sgml.gz
+
+Format: postscript
+Files: /usr/share/doc/onlyoffice-documentserver-plugin-chess/onlyoffice-documentserver-plugin-chess.ps.gz
+
+Format: text
+Files: /usr/share/doc/onlyoffice-documentserver-plugin-chess/onlyoffice-documentserver-plugin-chess.text.gz
+
+Format: HTML
+Index: /usr/share/doc/onlyoffice-documentserver-plugin-chess/html/index.html
+Files: /usr/share/doc/onlyoffice-documentserver-plugin-chess/html/*.html
