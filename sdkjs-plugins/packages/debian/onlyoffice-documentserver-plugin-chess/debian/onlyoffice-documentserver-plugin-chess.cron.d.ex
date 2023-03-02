50abffd2e70386bb4f010b75defe3af7bef9d3ad
Elbakyan Shirak <shirak.elbakyan@onlyoffice.com>
2023-03-02 15:24:03 +0400

    Deleted useless files, corrected FH & .install
    - Deleted unused files
    - Deleted files created after the build stage
    - Corrected file hierarchy
    - Fixed <packeg-name>.install to install files from github-repoes
    - Commit from a new branch correctly named

--- sdkjs-plugins/packages/debian/onlyoffice-documentserver-plugin-chess/debian/onlyoffice-documentserver-plugin-chess.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the onlyoffice-documentserver-plugin-chess package
-#
-0 4	* * *	root	[ -x /usr/bin/onlyoffice-documentserver-plugin-chess_maintenance ] && /usr/bin/onlyoffice-documentserver-plugin-chess_maintenance
