e0e79ab129b8923e32cc95b29091966c300970e3
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2023-04-12 14:56:35 +0300

    .

--- store/scripts/code.js
+++ store/scripts/code.js
@@ -1090,8 +1090,6 @@ function getImageUrl(guid, bNotForStore, bSetSize, id) {
 			let start;
 			if (isDesktop) {
 				baseUrl = plugin.obj.baseUrl;
-				start = baseUrl.indexOf('sdkjs-plugins');
-				baseUrl = '../../../' + baseUrl.substring(start);
 			} else {
 				baseUrl = plugin.baseUrl;
 				start = baseUrl.indexOf('web-apps');
