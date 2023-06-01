4444227f25edacfacdbab1b0ce8c6a8423fd39a2
Ivan Georgiev <dexycon_dev@dexycon.com>
2023-06-01 15:38:01 +0530

    Plugin created

---
+++ sdkjs-plugins/content/edcomment/plugin.js
@@ -0,0 +1,12 @@
+(function (window, undefined) {
+    window.Asc.plugin.init = function () {
+        this.callCommand(function() {
+            var oDocument = Api.GetDocument();
+            var oParagraph = Api.CreateParagraph();
+            oParagraph.AddText("Hello world!");
+            oDocument.InsertContent([oParagraph]);
+        }, true);
+    };
+    window.Asc.plugin.button = function (id) {
+    };
+})(window, undefined);
