2da82690f544931a3b452f96efa346b2a0630923
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2023-06-07 15:41:38 +0300

    .

--- sdkjs-plugins/content/openai/scripts/custom.js
+++ sdkjs-plugins/content/openai/scripts/custom.js
@@ -238,8 +238,7 @@
 			div.addEventListener('mouseenter', function (event) {
 				event.target.children[0].classList.remove('hidden');
 				let top = event.target.offsetTop - event.target.children[0].clientHeight - 5 + 'px;';
-				// let top = event.target.lastElementChild.offsetTop - event.target.lastElementChild.clientHeight - event.target.firstElementChild.clientHeight - (6 *devicePixelRatio) + 'px';
-				event.target.children[0].setAttribute('style', 'top: '+top+'');
+				event.target.children[0].setAttribute('style', 'top:' + top);
 			});
 
 			div.addEventListener('mouseleave', function (event) {
@@ -248,11 +247,11 @@
 		});
 
 		let modals = document.querySelectorAll('.div_title');
-		// modals.forEach(function(modal) {
-		// 	modal.addEventListener('mouseenter', function (event) {
-		// 		event.target.classList.add('hidden');
-		// 	});
-		// });
+		modals.forEach(function(modal) {
+			modal.addEventListener('mouseenter', function (event) {
+				event.target.classList.add('hidden');
+			});
+		});
 	};
 
 	function onSlInput(e) {
