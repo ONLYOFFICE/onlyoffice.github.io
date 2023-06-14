6684ec0bee182ffa7b876c16ba848dbcd7ffabf6
Chirag <chirag.encodedots@gmail.com>
2023-06-14 11:09:47 +0530

    Get the text of selection

--- sdkjs-plugins/content/propact_dev/scripts/code.js
+++ sdkjs-plugins/content/propact_dev/scripts/code.js
@@ -103,6 +103,16 @@
 			const formData = new FormData(formElem);
 			var contractSectionName = formData.get('contractsection');
 			var contractDescription = formData.get('contractdescription');
+
+			var selectedText = "";
+			if (window.getSelection) {
+				selectedText = window.getSelection().toString();
+			} else if (document.selection && document.selection.type != "Control") {
+				selectedText = document.selection.createRange().text;
+			}
+			console.log('selectedText', selectedText);
+
+
 			window.Asc.plugin.executeMethod ("AddContentControl", [2, {"Id" : 7, "Description": contractDescription, "Tag" : contractSectionName, "Lock" : 0, "Color" : { "R" : 0, "G" : 255, "B" : 0 }}]);
 		});
 
