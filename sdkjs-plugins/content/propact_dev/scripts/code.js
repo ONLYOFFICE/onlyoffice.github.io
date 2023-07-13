36958654fadd44b1673fd06d683857f87fd982c6
chiragencodedots <chirag.encodedots@gmail.com>
2023-07-13 16:29:00 +0530

    Comment the code

--- sdkjs-plugins/content/propact_dev/scripts/code.js
+++ sdkjs-plugins/content/propact_dev/scripts/code.js
@@ -37,16 +37,16 @@
 
 		// config ref
 		var config;
-
-		// Get ready to receive the response from TOP
-		window.parent.Common.Gateway.on ( 'internalcommand', ( data ) => {
-			if ( data.command === 'getConfig' ) {
-				config = data.data;
-			}
-		} );
-
-		// Send custom config request to TOP
-		window.parent.Common.Gateway.sendInfo ( { getConfig: true } );
+		//
+		// // Get ready to receive the response from TOP
+		// window.parent.Common.Gateway.on ( 'internalcommand', ( data ) => {
+		// 	if ( data.command === 'getConfig' ) {
+		// 		config = data.data;
+		// 	}
+		// } );
+		//
+		// // Send custom config request to TOP
+		// window.parent.Common.Gateway.sendInfo ( { getConfig: true } );
 
 
 		// $(document).ready(function () {
