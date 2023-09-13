f2a099ec7ddd8e4dc890e68a659e3f6d39e1f6df
Лазарев Вячеслав <lazarev@enersys.ru>
2023-09-13 17:21:50 +0300

    1 version

--- sdkjs-plugins/content/prostor/scripts/prostor.js
+++ sdkjs-plugins/content/prostor/scripts/prostor.js
@@ -1,129 +1,93 @@
-
-// Example insert text into editors (different implementations)
 (function (window, undefined) {
+   const Api = window.parent.g_asc_plugins.api;
 
-    // var text = "Всем ПРОСТОР!";
-    
+   function update() {
 
-    window.Asc.plugin.init = function () { // Обязательная функция window.Asc.plugin.init исполняется при инициализации выполнения плагина
-        // call command with external variables
-        // Asc.scope.text = text; // export variable to plugin scope
-        
-        this.callCommand(function () {
-            var endpoint = Api.GetActiveSheet().GetRange("A1").GetValue();
-            function jsonPath(obj, expr, arg) {
-                var P = {
-                   resultType: arg && arg.resultType || "VALUE",
-                   result: [],
-                   normalize: function(expr) {
-                      var subx = [];
-                      return expr.replace(/[\['](\??\(.*?\))[\]']/g, function($0,$1){return "[#"+(subx.push($1)-1)+"]";})
-                                 .replace(/'?\.'?|\['?/g, ";")
-                                 .replace(/;;;|;;/g, ";..;")
-                                 .replace(/;$|'?\]|'$/g, "")
-                                 .replace(/#([0-9]+)/g, function($0,$1){return subx[$1];});
-                   },
-                   asPath: function(path) {
-                      var x = path.split(";"), p = "$";
-                      for (var i=1,n=x.length; i<n; i++)
-                         p += /^[0-9*]+$/.test(x[i]) ? ("["+x[i]+"]") : ("['"+x[i]+"']");
-                      return p;
-                   },
-                   store: function(p, v) {
-                      if (p) P.result[P.result.length] = P.resultType == "PATH" ? P.asPath(p) : v;
-                      return !!p;
-                   },
-                   trace: function(expr, val, path) {
-                      if (expr) {
-                         var x = expr.split(";"), loc = x.shift();
-                         x = x.join(";");
-                         if (val && val.hasOwnProperty(loc))
-                            P.trace(x, val[loc], path + ";" + loc);
-                         else if (loc === "*")
-                            P.walk(loc, x, val, path, function(m,l,x,v,p) { P.trace(m+";"+x,v,p); });
-                         else if (loc === "..") {
-                            P.trace(x, val, path);
-                            P.walk(loc, x, val, path, function(m,l,x,v,p) { typeof v[m] === "object" && P.trace("..;"+x,v[m],p+";"+m); });
-                         }
-                         else if (/,/.test(loc)) { // [name1,name2,...]
-                            for (var s=loc.split(/'?,'?/),i=0,n=s.length; i<n; i++)
-                               P.trace(s[i]+";"+x, val, path);
-                         }
-                         else if (/^\(.*?\)$/.test(loc)) // [(expr)]
-                            P.trace(P.eval(loc, val, path.substr(path.lastIndexOf(";")+1))+";"+x, val, path);
-                         else if (/^\?\(.*?\)$/.test(loc)) // [?(expr)]
-                            P.walk(loc, x, val, path, function(m,l,x,v,p) { if (P.eval(l.replace(/^\?\((.*?)\)$/,"$1"),v[m],m)) P.trace(m+";"+x,v,p); });
-                         else if (/^(-?[0-9]*):(-?[0-9]*):?([0-9]*)$/.test(loc)) // [start:end:step]  phyton slice syntax
-                            P.slice(loc, x, val, path);
-                      }
-                      else
-                         P.store(path, val);
-                   },
-                   walk: function(loc, expr, val, path, f) {
-                      if (val instanceof Array) {
-                         for (var i=0,n=val.length; i<n; i++)
-                            if (i in val)
-                               f(i,loc,expr,val,path);
-                      }
-                      else if (typeof val === "object") {
-                         for (var m in val)
-                            if (val.hasOwnProperty(m))
-                               f(m,loc,expr,val,path);
-                      }
-                   },
-                   slice: function(loc, expr, val, path) {
-                      if (val instanceof Array) {
-                         var len=val.length, start=0, end=len, step=1;
-                         loc.replace(/^(-?[0-9]*):(-?[0-9]*):?(-?[0-9]*)$/g, function($0,$1,$2,$3){start=parseInt($1||start);end=parseInt($2||end);step=parseInt($3||step);});
-                         start = (start < 0) ? Math.max(0,start+len) : Math.min(len,start);
-                         end   = (end < 0)   ? Math.max(0,end+len)   : Math.min(len,end);
-                         for (var i=start; i<end; i+=step)
-                            P.trace(i+";"+expr, val, path);
-                      }
-                   },
-                   eval: function(x, _v, _vname) {
-                      try { return $ && _v && eval(x.replace(/@/g, "_v")); }
-                      catch(e) { throw new SyntaxError("jsonPath: " + e.message + ": " + x.replace(/@/g, "_v").replace(/\^/g, "_a")); }
-                   }
-                };
-             
-                var $ = obj;
-                if (expr && obj && (P.resultType == "VALUE" || P.resultType == "PATH")) {
-                   P.trace(P.normalize(expr).replace(/^\$;/,""), obj, "$");
-                   return P.result.length ? P.result : false;
-                }
-             } ;
-             
-            // var json_path_expr = Api.GetActiveSheet().GetRange("A2").GetValue();
-            // var url = 'http://'+server+'/v2/StationModels/getEntities?folder='+folder+'&withProps=true&withRefs=true&hierarchical=false';
-            fetch(endpoint)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                    console.log(Api.GetActiveSheet().GetRange("A2").GetValue());
-                    Api.GetActiveSheet().GetActiveCell().SetValue(data);
-                    var res = jsonPath(data, Api.GetActiveSheet().GetRange("A2").GetValue());
-                    console.log(res);
-                    console.log(Api.GetActiveSheet().GetActiveCell().GetRange());
-                    var nRow = 1;
-                    var oRange = sheet.GetRangeByNumber(5, 5);
-                    for(var r in res) {
-                        oRange = sheet.GetRangeByNumber(nRow, 0);
-                        oRange.SetValue(r);
-                        // oRange = sheet.GetRangeByNumber(nRow, 1);
-                        // oRange.SetValue(oDates[date]);
-                        nRow++;
-                    Api.GetActiveSheet().GetRange().SetValue();
+      // Чтение сохраненных ранее на скрытом листе настроек 
+      var ActiveSheet = Api.GetActiveSheet();
+      var ActiveSheetName = ActiveSheet.GetName();
+      // var plugin_sheet = Api.GetSheet("prostor_plugin_" + ActiveSheetName);
+      var plugin_sheet = Api.GetSheet("prostor_plugin");
+
+      if (plugin_sheet) {
+         var d = plugin_sheet.GetRange("A1:D100").GetValue2(); // Массив всех значений настроек запросов (история запросов задана жестко - не более 100 запросов)
+         let settings = {};
+         d.forEach(el => {
+            if (el[0] != "") {
+               settings.Sheet = el[3]; // Лист, для которого применяются настройки запросов               
+               if (settings.Sheet === ActiveSheetName) { // Проверяем для текущего ли листа эти настройки, есла да, то считываем остальные, нет - игнорим
+               settings.UrlRequest = el[0];
+               settings.JsonPathExpr = el[1];
+               settings.Range = el[2];
+               console.log("Запрос к НСИ " + settings.UrlRequest);
+               console.log("JsonPathExpr " + settings.JsonPathExpr);
+               console.log("Range " + settings.Range);
+
+               let xhr = new XMLHttpRequest();
+               xhr.open('GET', settings.UrlRequest, false);
+               
+               try {
+                 xhr.send();
+                 if (xhr.status != 200) {
+                   alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+                 } else {
+                  //  console.log(xhr.response);
+                   var ans = JSON.parse(xhr.responseText);
+                  //  console.log(ans);
+                   var jsonpathexprs = settings.JsonPathExpr.split(';'); // Парсим выражения jsonpath, написанные  через ; (точку с запятой)
+                   var res = [];
+                   jsonpathexprs.forEach(jsonpathexpr => {
+                      res.push(jsonPath(ans, jsonpathexpr));
+                   });
+
+                   Api.GetActiveSheet().GetRange(settings.Range).Clear();
+                   console.log("res " + res);
+                   Api.GetActiveSheet().GetRange(settings.Range).SetValue(res);
+                 }
+               } catch(err) { // для отлова ошибок используем конструкцию try...catch вместо onerror
+                 alert("Запрос не удался "+err);
+                 console.log(err);
+               }
+              
+               // fetch(settings.UrlRequest)
+               //    // Делаем запросы к API НСИ
+               //    .then(response => response.json())
+               //    .then(ans => {
+               //       var jsonpathexprs = settings.JsonPathExpr.split(';'); // Парсим выражения jsonpath, написанные  через ; (точку с запятой)
+               //       var res = [];
+               //       jsonpathexprs.forEach(jsonpathexpr => {
+
+               //          res.push(jsonPath(ans, jsonpathexpr));
+               //       });
+
+               //       Api.GetActiveSheet().GetRange(settings.Range).Clear();
+               //       console.log("res " + res);
+               //       Api.GetActiveSheet().GetRange(settings.Range).SetValue(res);
+                     
+               //    }
+               //    );
             }
-            )
-                .then(res =>  Api.GetActiveSheet().GetActiveCell().SetValue(res));
-        
-        }, true);
+         }
+         });
+         Api.Save();
+         console.log("Отчет обновлен!")
+      } else {
+         console.log("Нет сохраненных запросов")
+      };
+   };
 
-    };
 
-    window.Asc.plugin.button = function (id) { // обязательная функция window.Asc.plugin.button
 
-    };
+   window.Asc.plugin.init = function (text) { // Обязательная функция window.Asc.plugin.init исполняется при инициализации выполнения плагина
+      console.log("Выполняем обновление");
+      update();
+      this.executeCommand("close", "");
+   };
 
-})(window, undefined);
+   window.Asc.plugin.button = function (id) { // обязательная функция window.Asc.plugin.button
+      this.executeCommand("close", "");
+
+   };
+
+}
+)(window, undefined);
