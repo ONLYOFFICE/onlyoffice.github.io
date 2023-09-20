64c6e3656149013f07ce3d1e386e71d8c592d0fb
brick-pid <jianbo.lin@outlook.com>
2023-09-20 11:03:32 +0800

    refract code and update readme

--- sdkjs-plugins/content/zhipu/scripts/code.js
+++ sdkjs-plugins/content/zhipu/scripts/code.js
@@ -2,9 +2,8 @@
 (function (window, undefined) {
     let ApiKey = '';
     let hasKey = false;
-    const maxLen = 4000;
-    let messageHistory = null;
-    let conversationHistory = null;
+    let messageHistory = null; // a reference to the message history DOM element
+    let conversationHistory = null; // a list of all the messages in the conversation
     let messageInput = null;
     let typingIndicator = null;
 
@@ -14,13 +13,12 @@
             hasKey = true;
         } else {
             hasKey = false;
-            displayMessage('请先设置API Key', 'ai-message');
+            displayMessage(generateText('Set Your ZhiPu API Key first'), 'ai-message');
         }
     };
 
 
     window.Asc.plugin.init = function () {
-        // 插件初始化
         messageHistory = document.querySelector('.message-history');
         conversationHistory = [];
         typingIndicator = document.querySelector('.typing-indicator');
@@ -36,38 +34,54 @@
             guid: window.Asc.plugin.guid,
             items: [
                 {
-                    id: 'AiProcess',
-                    text: '智谱 AI Copilot',
+                    id: 'ZhiPu Copilot',
+                    text: generateText('ZhiPu Copilot'),
                     items: [
                         {
                             id: 'generate',
-                            text: '生成',
+                            text: generateText('generate'),
                         },
                         {
                             id: 'summarize',
-                            text: '总结',
+                            text: generateText('summarize'),
                         },
                         {
                             id: 'explain',
-                            text: '解释',
+                            text: generateText('explain'),
                         },
                         {
                             id: 'translate',
-                            text: '翻译',
+                            text: generateText('translate'),
                             items: [
                                 {
                                     id: 'translate_to_en',
-                                    text: '翻译为英文',
+                                    text: generateText('translate to English'),
                                 },
                                 {
                                     id: 'translate_to_zh',
-                                    text: '翻译为中文',
+                                    text: generateText('translate to Chinese'),
+                                },
+                                {
+                                    id: 'translate_to_fr',
+                                    text: generateText('translate to French'),
+                                },
+                                {
+                                    id: 'translate_to_de',
+                                    text: generateText('translate to German'),
+                                },
+                                {
+                                    id: 'translate_to_ru',
+                                    text: generateText('translate to Russian'),
+                                },
+                                {
+                                    id: 'translate_to_es',
+                                    text: generateText('translate to Spanish'),
                                 }
                             ]
                         },
                         {
                             id: 'clear_history',
-                            text: '清空对话历史',
+                            text: generateText('clear chat history'),
                         }
                     ]
                 }
@@ -88,14 +102,14 @@
     });
 
     const displayMessage = function (message, messageType) {
-        message = message.replace(/^"|"$/g, ''); // 去除首尾的双引号
-        message = message.replace(/\\n/g, '\n'); // 将字面上的换行符替换为真正的换行符
+        message = message.replace(/^"|"$/g, ''); // remove surrounding quotes
+        message = message.replace(/\\n/g, '\n'); // replace \n with newline characters
 
-        // 创建新的消息元素
+        // create a new message element
         const messageElement = document.createElement('div');
         messageElement.classList.add(messageType); // Add a class for user messages
 
-        // 将消息分割为行，并为每一行创建一个文本节点和一个换行符
+        // split the message into lines and create a text node for each line
         const lines = message.split('\n');
         for (const line of lines) {
             const textNode = document.createTextNode(line);
@@ -103,120 +117,183 @@
             messageElement.appendChild(document.createElement('br'));
         }
 
-        // 将新消息添加到历史消息区域
+        // add the message element to the message history
         messageHistory.appendChild(messageElement);
 
-        // 滚动到最新的消息
+        //  scroll to the bottom of the message history
         messageHistory.scrollTop = messageHistory.scrollHeight;
 
-        conversationHistory.push({ role: messageType === 'user-message' ? 'user' : 'assistant', content: message });
-        // console.log("对话历史:", JSON.stringify(conversationHistory));
+        console.log("history: ", conversationHistory);
     };
 
 
 
-    // 总结
+    // summarize
     window.Asc.plugin.attachContextMenuClickEvent('summarize', function () {
         window.Asc.plugin.executeMethod('GetSelectedText', null, function (text) {
             conversationHistory.push({ role: 'user', content: '总结下面的文本' + text });
-            let response = generateResponse();
-            response.then(function (res) {
-                displayMessage(res, 'ai-message');
-            });
+            generateResponse()
+                .then(function (res) {
+                    displayMessage(res, 'ai-message');
+                    conversationHistory.push({ role: 'assistant', content: res });
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        });
+    });
+
+    // explain 
+    window.Asc.plugin.attachContextMenuClickEvent('explain', function () {
+        window.Asc.plugin.executeMethod('GetSelectedText', null, function (text) {
+            conversationHistory.push({ role: 'user', content: '解释下面的文本' + text });
+            generateResponse()
+                .then(function (res) {
+                    displayMessage(res, 'ai-message');
+                    conversationHistory.push({ role: 'assistant', content: res });
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         });
     });
 
     const translateHelper = function (text, targetLanguage) {
-        console.log(`翻译为${targetLanguage}选中的文本：`, text);
+        console.log(`将下面的文本翻译为${targetLanguage}：`, text);
         conversationHistory.push({ role: 'user', content: `将下面的文本翻译为${targetLanguage}：` + text });
-        let response = generateResponse();
-        response.then(function (res) {
-            // console.log(`翻译为${targetLanguage}-响应: `, res);
-            displayMessage(res, 'ai-message');
-        });
+        generateResponse()
+            .then(function (res) {
+                displayMessage(res, 'ai-message');
+                conversationHistory.push({ role: 'assistant', content: res });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
-    // 翻译为中文
+    // translate into Chinese
     window.Asc.plugin.attachContextMenuClickEvent('translate_to_zh', function () {
         window.Asc.plugin.executeMethod('GetSelectedText', null, function (text) {
             translateHelper(text, "中文");
         });
     });
 
-    // 翻译为英文
+    // translate into English
     window.Asc.plugin.attachContextMenuClickEvent('translate_to_en', function () {
         window.Asc.plugin.executeMethod('GetSelectedText', null, function (text) {
             translateHelper(text, "英文");
         });
     });
 
-    // 生成
+    // translate to French
+    window.Asc.plugin.attachContextMenuClickEvent('translate_to_fr', function () {
+        window.Asc.plugin.executeMethod('GetSelectedText', null, function (text) {
+            translateHelper(text, "法文");
+        });
+    });
+
+    // translate to German
+    window.Asc.plugin.attachContextMenuClickEvent('translate_to_de', function () {
+        window.Asc.plugin.executeMethod('GetSelectedText', null, function (text) {
+            translateHelper(text, "德文");
+        });
+    });
+
+    // translate to Russian
+    window.Asc.plugin.attachContextMenuClickEvent('translate_to_ru', function () {
+        window.Asc.plugin.executeMethod('GetSelectedText', null, function (text) {
+            translateHelper(text, "俄文");
+        });
+    });
+
+    // translate to spanish
+    window.Asc.plugin.attachContextMenuClickEvent('translate_to_es', function () {
+        window.Asc.plugin.executeMethod('GetSelectedText', null, function (text) {
+            translateHelper(text, "西班牙文");
+        });
+    });
+
+    // generate content directly in document
     window.Asc.plugin.attachContextMenuClickEvent('generate', function () {
         window.Asc.plugin.executeMethod('GetSelectedText', null, function (text) {
             conversationHistory.push({ role: 'user', content: '请根据指令生成对应文本：' + text });
-            let response = generateResponse();
-            response.then(function (res) {
-                // console.log("获得回复：", res);
-                conversationHistory.push({ role: 'assistant', content: res });
-                Asc.scope.paragraphs = res.slice(1, -1).split('\\n');
-                Asc.scope.st = Asc.scope.paragraphs;
-                Asc.plugin.callCommand(function () {
-                    var oDocument = Api.GetDocument();
-                    for (var i = 0; i < Asc.scope.st.length; i++) {
-                        var oParagraph = Api.CreateParagraph();
-                        oParagraph.AddText(Asc.scope.st[i]);
-                        oDocument.InsertContent([oParagraph]);
-                    }
-                }, false);
-            });
+            generateResponse()
+                .then(function (res) {
+                    conversationHistory.push({ role: 'assistant', content: res });
+                    Asc.scope.paragraphs = res.slice(1, -1).split('\\n');
+                    Asc.scope.st = Asc.scope.paragraphs;
+                    Asc.plugin.callCommand(function () {
+                        var oDocument = Api.GetDocument();
+                        for (var i = 0; i < Asc.scope.st.length; i++) {
+                            var oParagraph = Api.CreateParagraph();
+                            oParagraph.AddText(Asc.scope.st[i]);
+                            oDocument.InsertContent([oParagraph]);
+                        }
+                    }, false);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         });
 
     });
 
-
-    // 生成异步请求（用于in-doc功能）
-    let generateResponse = async function () {
-        let prompt = {
-            "prompt": conversationHistory
-        }
-        let res = await window.Asc.sendRequest(prompt);
-        console.log("获得回复：", res)
-        return res;
+    // generate async request (for in-doc function)
+    let generateResponse = function () {
+        return new Promise(function (resolve, reject) {
+            let prompt = {
+                "prompt": conversationHistory
+            }
+            window.Asc.sendRequest(prompt)
+                .then(function (res) {
+                    console.log("获得回复：", res);
+                    resolve(res);
+                })
+                .catch(function (error) {
+                    reject(error);
+                });
+        });
     }
 
     // Make sure the DOM is fully loaded before running the following code
     document.addEventListener("DOMContentLoaded", function () {
-        // 获取相关的DOM元素
+        // get references to the DOM elements
         messageInput = document.querySelector('.message-input');
         const sendButton = document.querySelector('.send-button');
         typingIndicator = document.querySelector('.typing-indicator');
 
-        // 发送消息的处理函数
-        async function sendMessage() {
+        // send a message when the user clicks the send button
+        function sendMessage() {
             const message = messageInput.value;
             if (message.trim() !== '') {
-                displayMessage(message, 'user-message'); // 创建新的用户消息元素
-                messageInput.value = ''; // 清空输入框
-                typingIndicator.style.display = 'block'; // 显示等待指示器
-                // const aiResponse = await generateResponse(); //生成AI回复
-                const reader = await sseRequest(conversationHistory);
-                typingIndicator.style.display = 'none'; // 隐藏等待指示器
-                // displayMessage(aiResponse, 'ai-message'); // 创建新的AI消息元素
-                displaySSEMessage(reader);
+                displayMessage(message, 'user-message');
+                conversationHistory.push({ role: 'user', content: message });
+                messageInput.value = ''; 
+                typingIndicator.style.display = 'block'; // display the typing indicator
+                sseRequest(conversationHistory)
+                    .then(reader => {
+                        console.log("SSE请求成功");
+                        let currentDiv = null;
+                        let currentMessage = null;
+                        displaySSEMessage(reader, currentDiv, currentMessage);
+                    })
+                    .catch(error => {
+                        console.log("SSE请求失败", error);
+                    });
+                typingIndicator.style.display = 'none'; // hide the typing indicator
             }
         }
 
-        // 绑定发送按钮的点击事件
         sendButton.addEventListener('click', sendMessage);
 
         messageInput.addEventListener('keydown', function (event) {
             if (event.key === 'Enter') {
-                event.preventDefault();  // 取消默认的Enter键事件
+                event.preventDefault();  // prevent the default behavior of the Enter key
                 if (event.shiftKey) {
-                    // 如果用户按下Shift+Enter，插入一个换行符
+                    // if the user pressed Shift+Enter, insert a newline character
                     messageInput.value += '\n';
                 } else {
-                    // 如果用户只按下Enter，发送消息
+                    // if the user only pressed Enter, send the message
                     sendMessage();
                 }
             }
@@ -224,65 +301,64 @@
     });
 
     function clearHistory() {
-        // 清空消息历史记录
         messageHistory.innerHTML = '';
-
-        // 清空对话历史记录
         conversationHistory = [];
-
-        // 清空输入框
         messageInput.value = '';
     }
 
 
-    async function sseRequest(conversationHistory) {
-        console.log("history: ", conversationHistory);
-        const jwt = window.Asc.JWT;
-        // 向服务器发送请求
-        console.log("SSE请求开始");
-        const model = localStorage.getItem('model');
-        const url = `https://open.bigmodel.cn/api/paas/v3/model-api/${model}/sse-invoke`;
+    function sseRequest(conversationHistory) {
+        return new Promise((resolve, reject) => {
+            console.log("history: ", conversationHistory);
+            const jwt = window.Asc.JWT;
+            console.log("SSE请求开始");
+            const model = localStorage.getItem('model');
+            const url = `https://open.bigmodel.cn/api/paas/v3/model-api/${model}/sse-invoke`;
 
-        // 请求头
-        const headers = {
-            'Accept': 'text/event-stream',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + jwt
-        };
+            const headers = {
+                'Accept': 'text/event-stream',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + jwt
+            };
 
-        // 请求体数据
-        const requestData = {
-            prompt: conversationHistory
-        };
+            const requestData = {
+                prompt: conversationHistory
+            };
 
-        // 发送POST请求获取响应
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(requestData)
+            fetch(url, {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(requestData)
+            })
+                .then(response => {
+                    const reader = response.body
+                        .pipeThrough(new TextDecoderStream())
+                        .getReader();
+                    resolve(reader);
+                })
+                .catch(err => {
+                    reject(err);
+                });
         });
-
-        return response.body.pipeThrough(new TextDecoderStream()).getReader();
     }
 
-    // 将SSE结果显示在页面上
-    async function displaySSEMessage(reader) {
-        let currentDiv = null;
-        let currentMessage = null;
-        while (true) {
-            const { value, done } = await reader.read();
-            if (done) break;
-            if (value.includes('finish' || 'error' || 'interrupt')) {
-                console.log(value);
-                currentDiv = null;
-                break;
+    function displaySSEMessage(reader, currentDiv, currentMessage) {
+        reader.read().then(function processResult(result) {
+            // console.log("stream result: ", result);
+            if (result.value.includes('event:end') || result.value.includes('event:error') || result.value.includes('event:interrupt') || result.value.includes('event:finish')) {
+                // console.log("result.value of stream", result.value);
+                conversationHistory.push({ role: 'assistant', content: currentMessage });
+                return;
             }
             if (currentDiv === null) {
                 currentDiv = document.createElement('div');
                 currentDiv.classList.add('ai-message');
                 messageHistory.appendChild(currentDiv);
-            };
-            const lines = value.split('\n');
+            }
+            if (currentMessage === null) {
+                currentMessage = '';
+            }
+            const lines = result.value.split('\n');
             lines.forEach(line => {
                 if (line.includes('data')) {
                     const fragment = line.split(':')[1];
@@ -294,10 +370,20 @@
                     }
                 }
             });
-        }
 
-        conversationHistory.push({ role: 'assistant', content: currentMessage });
+            // recursively call processResult() to continue reading data from the stream
+            displaySSEMessage(reader, currentDiv, currentMessage);
+        });
     }
 
+
+    function generateText(text) {
+        let lang = window.Asc.plugin.info.lang.substring(0, 2);
+        console.log("current lang: ", lang);
+        return {
+            en: text,
+            [lang]: window.Asc.plugin.tr(text)
+        }
+    };
 
 })(window, undefined);
