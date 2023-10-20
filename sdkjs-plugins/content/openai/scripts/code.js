6d7e3e48f0aea1d78fc3b90ba3d387099d57c5c0
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2023-10-20 15:21:57 +0300

    [openai]Fix error.

--- sdkjs-plugins/content/openai/scripts/code.js
+++ sdkjs-plugins/content/openai/scripts/code.js
@@ -533,12 +533,12 @@
 				break;
 
 			case 5:
-				settings.messages = [ { role: 'user', text];
+				settings.messages = [ { role: 'user', content: text } ];
 				url = 'https://api.openai.com/v1/chat/completions';
 				break;
 
 			case 6:
-				settings.messages = [ { role: 'user', text];
+				settings.messages = [ { role: 'user', content: text } ];
 				url = 'https://api.openai.com/v1/chat/completions';
 				break;
 
