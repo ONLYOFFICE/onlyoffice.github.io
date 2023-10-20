dd9895ec500837d7e1381878227a45e9090e89ae
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2023-10-20 15:10:31 +0300

    [openai] Fix error.

--- sdkjs-plugins/content/openai/scripts/custom.js
+++ sdkjs-plugins/content/openai/scripts/custom.js
@@ -318,7 +318,7 @@
 			obj.error = true;
 		} else {
 			if (obj.isChat)
-				obj.messages = [{role: 'user', message: value}];
+				obj.messages = [{role: 'user', content: value}];
 			else
 				obj.prompt = value;
 
