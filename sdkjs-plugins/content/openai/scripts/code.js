7e5d424939e452c6788d700326fe075928c8552b
AlexeyMatveev686 <Alexey.Matveev@onlyoffice.com>
2023-10-20 17:09:42 +0300

    [openai] Test

--- sdkjs-plugins/content/openai/scripts/code.js
+++ sdkjs-plugins/content/openai/scripts/code.js
@@ -884,7 +884,7 @@
 
 			case 'onExecuteMethod':
 				window.Asc.plugin.executeMethod(message.method, [message.data], function() {
-					window.Asc.plugin.executeMethod('CloseWindow', [modal.id]);
+					setTimeout(function(){window.Asc.plugin.executeMethod('CloseWindow', [modal.id])},0);
 				});
 				break;
 
