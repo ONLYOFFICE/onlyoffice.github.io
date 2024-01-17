4b6d7509d20eff282d7f7e955988310345288124
Serg-Kv <Sergei.Kosyrev@onlyoffice.com>
2024-01-17 18:57:14 +0500

    The InsertQr plugin added

---
+++ sdkjs-plugins/content/InsertQR/scripts/settings.js
@@ -0,0 +1,58 @@
+/**
+ *
+ * (c) Copyright Ascensio System SIA 2020
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+(function (window, undefined) {
+  window.Asc.plugin.init = function () {
+      let lang = window.Asc.plugin.info.lang.substring(0, 2);
+      const submitButton = document.getElementById('submitButton');
+      const textElement = document.getElementById('text');
+      const anchorElement = document.querySelector('#learnMore a');
+
+      function translateElements() {
+          if (lang !== "en") {
+              window.Asc.plugin.onTranslate = function () {
+                  submitButton.innerText = window.Asc.plugin.tr('Submit');
+                  textElement.placeholder = window.Asc.plugin.tr('Insert API key');
+                  anchorElement.innerText = window.Asc.plugin.tr('Learn more about API Ninjas');
+              };
+          }
+      }
+
+
+      const form = document.getElementById('SubmitForm');
+      translateElements();
+      form.addEventListener('submit', function (event) {
+          event.preventDefault();
+
+          // Get the API key value from the input element
+          const apiKey = textElement.value;
+
+         // Remove the text element from the window
+         textElement.remove();
+
+          // Disable the submit button
+          submitButton.disabled = true;
+
+          // Clear the input value
+          textElement.value = "";
+
+          // Save the API key to local storage
+          localStorage.setItem('ApiNinjasApiKey', apiKey);
+      });
+  };
+})(window, undefined);
