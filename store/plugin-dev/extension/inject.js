c566258150dffa0bb8b091d9512f34159139b6f2
sanjeevareddynagireddy <159939850+sanjeevareddynagireddy@users.noreply.github.com>
2024-02-13 13:33:18 -0500

    ok
    oktotest

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,5 +1,5 @@
 (function(window, undefined) {
-	var URL_TO_PLUGIN = "";
+	var URL_TO_PLUGIN = "https://sanjeevareddynagireddy.github.io/onlyoffice.github.io/sdkjs-plugins/content/helloworld/";
 	
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
