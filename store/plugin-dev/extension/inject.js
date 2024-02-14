f377da35ba06bc5d9da9d4e1a19d36e61a266e58
sanjeevareddynagireddy <159939850+sanjeevareddynagireddy@users.noreply.github.com>
2024-02-13 20:14:09 -0500

    Update inject.js
    ok

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,8 +1,9 @@
 (function(window, undefined) {
-	response.setHeader("Access-Control-Allow-Origin", "*");
+	
 	var URL_TO_PLUGIN ="https://github.com/sanjeevareddynagireddy/onlyoffice.github.io/tree/master/sdkjs-plugins/content/helloworld/";
 	
 	var xhrObj = new XMLHttpRequest();
+xhrObj.setHeader("Access-Control-Allow-Origin", "*");
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
 	xhrObj.send('');
 
