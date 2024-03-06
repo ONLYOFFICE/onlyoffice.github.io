eeb27df31c769c635e64612f41023d0f00a288a4
Serg-Kv <Sergei.Kosyrev@onlyoffice.com>
2024-03-06 21:10:57 +0500

    ie_warning.html and code_ie.js(closing issue) modified

--- sdkjs-plugins/content/insertQR/scripts/code_ie.js
+++ sdkjs-plugins/content/insertQR/scripts/code_ie.js
@@ -31,6 +31,7 @@
  */
 (function(window, undefined) {
 	   let messageWindow = null;
+	   let variation;
 	// let bTranslation = false;
 	// let bInit = false
 
@@ -72,7 +73,7 @@
 		let start = location.pathname.lastIndexOf('/') + 1;
 		let file = location.pathname.substring(start);
 
-		let variation = {
+		 variation = {
 			url : location.href.replace(file, 'ie_warning.html'),
 			description : (window.Asc.plugin.tr('Warning') || 'Warning'),
 			isVisual : true,
@@ -81,11 +82,12 @@
 			EditorsSupport : ["word", "slide"],
 			size : [ 592, 170 ]
 		};
+	}
 
 		if (!messageWindow) {
 			messageWindow = new window.Asc.PluginWindow();
 		}
 		messageWindow.show(variation);
-	}
+
 	
 })(window, undefined);
