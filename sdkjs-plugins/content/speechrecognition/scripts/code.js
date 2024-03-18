8383338e91c4d462bdf0dc2cff087f559d720806
Alexey Matveev <Alexey.Matveev@onlyoffice.com>
2024-03-18 13:20:02 +0300

    [speechrecognation] Add logs and change how we create an instance of class. Also add new event listener for "nomatch" event.

--- sdkjs-plugins/content/speechrecognition/scripts/code.js
+++ sdkjs-plugins/content/speechrecognition/scripts/code.js
@@ -20,6 +20,7 @@
 	var curLang;
 	var oTheme = null;
 	var defaultLang = "en-US";
+	var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 	
 	// If you modify this array, also update default language / dialect below.
 	var langs =
@@ -171,7 +172,7 @@
 			}
 		}
 
-		if (typeof(webkitSpeechRecognition) == "undefined") {
+		if (typeof(SpeechRecognition) == "undefined") {
 			alert('Web Speech API is not supported by this browser. Please open it in Google Chrome browser.');
 			document.getElementById("div_main").style.display = "none";
 			return;
@@ -216,11 +217,11 @@
 		recognizing = false,
 		start_timestamp;
 
-		if (!('webkitSpeechRecognition' in window)) {
+		if ( !('webkitSpeechRecognition' in window) && !('SpeechRecognition' in window) ) {
 			upgrade();
 		} else {
 			start_button.style.display = 'inline-block';
-			var recognition = new webkitSpeechRecognition();
+			var recognition = new SpeechRecognition();
 			recognition.continuous = true;
 			recognition.interimResults = true;
 
@@ -258,7 +259,13 @@
 						alert('Permission to use microphone was denied.');
 					}
 				}
+				console.error(error);
 			};
+
+			recognition.onnomatch = function(event) {
+				console.log('no much recognition');
+				console.log(event);
+			}
 
 			recognition.onend = function() {
 				if (!ignore_onend) {
