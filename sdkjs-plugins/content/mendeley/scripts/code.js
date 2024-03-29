bb77f6ec69b32cee7f49a9faa106c68dddd1ff8d
Alexey Matveev <Alexey.Matveev@onlyoffice.com>
2024-03-29 16:44:51 +0300

    [mendeley] Fix problem with translations.

--- sdkjs-plugins/content/mendeley/scripts/code.js
+++ sdkjs-plugins/content/mendeley/scripts/code.js
@@ -154,8 +154,6 @@
             authFlow: authFlow
         });
 
-        window.Asc.plugin.onTranslate = applyTranslations;
-
         redirectUrl = document.location.protocol + "//" + document.location.host + document.location.pathname.replace("index.html", "oauth.html");
         elements.redirectConfigUrl.value = redirectUrl;
 
@@ -509,7 +507,7 @@
         };
     };
 
-    function applyTranslations() {
+    window.Asc.plugin.onTranslate = function() {
         var elements = document.getElementsByClassName("i18n");
 
         for (var i = 0; i < elements.length; i++) {
@@ -517,7 +515,7 @@
             if (el.attributes["placeholder"]) el.attributes["placeholder"].value = getMessage(el.attributes["placeholder"].value);
             if (el.innerText) el.innerText = getMessage(el.innerText);
         }
-    }
+    };
 
     function saveLastUsedStyle(id) {
         localStorage.setItem("mendStyleId", id);
