28461a485e267c99338f72564d09d3c81e31efdb
Serg-Kv <Sergei.Kosyrev@onlyoffice.com>
2024-04-04 19:18:33 +0500

    QR Settings name changed

--- sdkjs-plugins/content/insertQR/scripts/code.js
+++ sdkjs-plugins/content/insertQR/scripts/code.js
@@ -149,7 +149,7 @@
       case 'displaySettings':
         variation = {
           url: location.href.replace(file, 'settingsQR.html'),
-          description: generateText('Settings'),
+          description: generateText('QR Settings'),
           isVisual: true,
           isModal: true,
           buttons: [],
