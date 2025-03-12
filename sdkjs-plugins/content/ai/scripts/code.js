3964a52ddbd15699eeaba05c4392078ffa8cb896
Oleg Korshul <Oleg.Korshul@onlyoffice.com>
2025-03-12 21:31:21 +0300

    Refactoring docked event

--- sdkjs-plugins/content/ai/scripts/code.js
+++ sdkjs-plugins/content/ai/scripts/code.js
@@ -12,27 +12,15 @@ function initWithTranslate() {
 	}
 }
 
-
-function clearChatState(frameId) {
-	let key = 'onlyoffice_ai_chat_state-' + frameId;
-	if(window.localStorage.getItem(key)) {
+function clearChatState() {
+	let key = 'onlyoffice_ai_chat_state';
+	if (window.localStorage.getItem(key))
 		window.localStorage.removeItem(key);
-	}
-}
-
-function clearAllChatsState() {
-	let localStorageArr = window.localStorage;
-	for (let i = 0; i < localStorageArr.length; i++) {
-		let key = window.localStorage.key(i);
-		if(key.includes('onlyoffice_ai_chat_state')) {
-			window.localStorage.removeItem(key);
-		}
-	}
 }
 
 window.Asc.plugin.init = function() {
 	initWithTranslate();
-	clearAllChatsState();
+	clearChatState();
 };
 
 window.Asc.plugin.onTranslate = function() {
@@ -42,6 +30,12 @@ window.Asc.plugin.onTranslate = function() {
 window.Asc.plugin.button = function(id, windowId) {
 	if (!windowId) {
 		return
+	}
+
+	if (window.chatWindow && window.chatWindow.id === windowId)
+	{
+		clearChatState();
+		delete window.chatWindow;
 	}
 
 	if (settingsWindow && windowId === settingsWindow.id) {
@@ -60,9 +54,6 @@ window.Asc.plugin.button = function(id, windowId) {
 		}
 	} else {
 		window.Asc.plugin.executeMethod("CloseWindow", [windowId]);
-		
-		// Clear state of the AI chat
-		clearChatState(windowId);
 	}
 };
 
