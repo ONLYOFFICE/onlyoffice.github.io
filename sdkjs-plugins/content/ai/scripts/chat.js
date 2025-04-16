39f3d071ce6f86759118c2f11d64525317438aae
Alexey Koshelev <Basher0303@yandex.ru>
2025-04-16 14:59:34 +0300

    Fix tooltips for chatbot

--- sdkjs-plugins/content/ai/scripts/chat.js
+++ sdkjs-plugins/content/ai/scripts/chat.js
@@ -115,7 +115,7 @@
 					$messageContent.append($error);
 				} else {
 					let $actionButtons = $('<div class="action_buttons_list"></div>');
-					actionButtons.forEach(function(button) {
+					actionButtons.forEach(function(button, index) {
 						let buttonEl = $('<button class="action_button btn-text-default"></button>');
 						buttonEl.append('<img class="icon" src="' + getFormattedPathForIcon(button.icon) + '"/>');
 						buttonEl.on('click', function() {
@@ -123,7 +123,10 @@
 						});
 		
 						if(button.tipOptions) {
-							new Tooltip(buttonEl[0], button.tipOptions);
+							if(item.btnTips[index]) {
+								item.btnTips[index]._deleteTooltipElement();
+							}
+							item.btnTips[index] = new Tooltip(buttonEl[0], button.tipOptions);
 						}
 						
 						$actionButtons.append(buttonEl);
@@ -183,6 +186,7 @@
 			if(message.role == 'assistant') {
 				message.activeContentIndex = 0;
 			}
+			message.btnTips = [];
 			this._list.push(message)
 			this._renderItemToList(message, this._list.length - 1);
 		},
