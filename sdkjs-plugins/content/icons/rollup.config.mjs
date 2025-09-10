496dcb69686f019006e39a4deff0a935918817e6
Artur <artur.drapeza@onlyoffice.com>
2025-09-10 13:24:09 +0300

    Different bundles for old and modern browsers

--- sdkjs-plugins/content/icons/rollup.config.mjs
+++ sdkjs-plugins/content/icons/rollup.config.mjs
@@ -1,33 +1,54 @@
-import { babel } from '@rollup/plugin-babel';
+import { babel } from "@rollup/plugin-babel";
+
+const isES5Build = process.env.TARGET === "es5";
+
+function getBabelConfig() {
+  if (isES5Build) {
+    return {
+      babelHelpers: "bundled",
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: {
+              browsers: ["> 0.5%", "last 2 versions", "IE 11"],
+            },
+            modules: false, // Rollup processes modules itself
+            useBuiltIns: "usage",
+            corejs: 3,
+          },
+        ],
+      ],
+      plugins: ["@babel/plugin-transform-class-properties"],
+    };
+  }
+
+  return {
+    babelHelpers: "bundled",
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          targets: { esmodules: true },
+          modules: false,
+        },
+      ],
+    ],
+  };
+}
 
 export default {
   input: "src/index.js",
   output: {
-    file: "dist/bundle.es5.js",
-    format: "umd",
-    name: "icons",
+    file: isES5Build ? "dist/bundle.es5.js" : "dist/bundle.modern.js",
+    format: isES5Build ? "umd" : "esm",
+    name: isES5Build ? "Icons" : undefined,
     sourcemap: true,
   },
   plugins: [
     babel({
-      babelHelpers: "bundled",
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              browsers: ['> 0.5%', 'last 2 versions', 'IE 11']
-            },
-            modules: false, // Rollup processes modules itself
-            useBuiltIns: 'usage',
-            corejs: 3 // Не забудьте установить core-js
-          }
-        ]
-      ],
+      ...getBabelConfig(),
       exclude: "node_modules/**",
-      plugins: [
-        '@babel/plugin-transform-class-properties'
-      ],
     }),
   ],
 };
