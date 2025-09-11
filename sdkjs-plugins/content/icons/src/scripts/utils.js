d74eb8db8c77c36e2507142f4f59898a0fa1d528
Artur <artur.drapeza@onlyoffice.com>
2025-09-11 14:26:08 +0300

    Ability to choose a category

---
+++ sdkjs-plugins/content/icons/src/scripts/utils.js
@@ -0,0 +1,12 @@
+class Utils {
+  static escapeHtml(unsafe) {
+    return unsafe
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+  }
+}
+
+export { Utils };
