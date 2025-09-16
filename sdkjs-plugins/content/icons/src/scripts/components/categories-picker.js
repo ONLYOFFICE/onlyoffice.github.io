199be7e29b991bff3be92a88d0548a216d3969ed
Artur <artur.drapeza@onlyoffice.com>
2025-09-16 11:29:20 +0300

    changed the boot order

--- sdkjs-plugins/content/icons/src/scripts/components/categories-picker.js
+++ sdkjs-plugins/content/icons/src/scripts/components/categories-picker.js
@@ -1,81 +1,81 @@
 class CategoriesPicker {
-  #container;
-  #onSelectCategoryCallback = () => {};
-  #selectedCategory = "";
+    #container;
+    #onSelectCategoryCallback = () => {};
+    #selectedCategory = "";
 
-  constructor(catalogOfIcons, containerId) {
-    this.#container = document.getElementById(containerId);
-    this.#addEventListener();
-    this.#show(catalogOfIcons);
-  }
+    constructor(catalogOfIcons) {
+        this.#container = document.getElementById("categories");
+        this.#addEventListener();
+        this.#show(catalogOfIcons);
+    }
 
-  #show(catalogOfIcons) {
-    this.#selectedCategory = "";
-    const fragment = document.createDocumentFragment();
+    #show(catalogOfIcons) {
+        this.#selectedCategory = "";
+        const fragment = document.createDocumentFragment();
 
-    catalogOfIcons.forEach((categoryInfo) => {
-      let id = categoryInfo.id;
-      let label = categoryInfo.label;
+        catalogOfIcons.forEach((categoryInfo) => {
+            let id = categoryInfo.id;
+            let label = categoryInfo.label;
 
-      const categoryContainer = document.createElement("div");
-      fragment.appendChild(categoryContainer);
+            const categoryContainer = document.createElement("div");
+            fragment.appendChild(categoryContainer);
 
-      categoryContainer.className = "category";
-      const categoryName = document.createElement("span");
-      categoryContainer.appendChild(categoryName);
-      categoryName.textContent = label;
-      categoryName.setAttribute("data-id", id);
-      categoryName.className = "category-name";
-    });
+            categoryContainer.className = "category";
+            const categoryName = document.createElement("span");
+            categoryContainer.appendChild(categoryName);
+            categoryName.textContent = label;
+            categoryName.setAttribute("data-id", id);
+            categoryName.className = "category-name";
+        });
 
-    this.#container.appendChild(fragment);
-  }
+        this.#container.appendChild(fragment);
+    }
 
-  reset() {
-    if (this.#selectedCategory !== "") {
-      this.#selectedCategory = "";
-      this.#container
-        .querySelectorAll(".category.selected")
-        .forEach((category) => {
-          category.classList.remove("selected");
+    reset() {
+        if (this.#selectedCategory !== "") {
+            this.#selectedCategory = "";
+            this.#container
+                .querySelectorAll(".category.selected")
+                .forEach((category) => {
+                    category.classList.remove("selected");
+                });
+        }
+    }
+
+    /**
+     * Set the callback function to be called when a category is selected.
+     * The callback function will receive the name of the selected category as a parameter.
+     * @param {function} callback - The callback function to be called when a category is selected
+     */
+    setOnSelectCategoryCallback(callback) {
+        this.#onSelectCategoryCallback = callback;
+    }
+
+    #addEventListener() {
+        this.#container.addEventListener("click", (e) => {
+            const categoryName = e.target.closest(".category-name");
+            if (categoryName) {
+                let id = categoryName.getAttribute("data-id");
+                let category = categoryName.parentElement;
+                let wasSelected = category.classList.contains("selected");
+
+                this.#container
+                    .querySelectorAll(".category.selected")
+                    .forEach((category) => {
+                        category.classList.remove("selected");
+                    });
+
+                if (wasSelected) {
+                    category.classList.remove("selected");
+                    this.#selectedCategory = "";
+                } else {
+                    category.classList.add("selected");
+                    this.#selectedCategory = id;
+                }
+                this.#onSelectCategoryCallback(this.#selectedCategory);
+            }
         });
     }
-  }
-
-  /**
-   * Set the callback function to be called when a category is selected.
-   * The callback function will receive the name of the selected category as a parameter.
-   * @param {function} callback - The callback function to be called when a category is selected
-   */
-  setOnSelectCategoryCallback(callback) {
-    this.#onSelectCategoryCallback = callback;
-  }
-
-  #addEventListener() {
-    this.#container.addEventListener("click", (e) => {
-      const categoryName = e.target.closest(".category-name");
-      if (categoryName) {
-        let id = categoryName.getAttribute("data-id");
-        let category = categoryName.parentElement;
-        let wasSelected = category.classList.contains("selected");
-
-        this.#container
-          .querySelectorAll(".category.selected")
-          .forEach((category) => {
-            category.classList.remove("selected");
-          });
-
-        if (wasSelected) {
-          category.classList.remove("selected");
-          this.#selectedCategory = "";
-        } else {
-          category.classList.add("selected");
-          this.#selectedCategory = id;
-        }
-        this.#onSelectCategoryCallback(this.#selectedCategory);
-      }
-    });
-  }
 }
 
 export { CategoriesPicker };
--- sdkjs-plugins/content/icons/src/scripts/components/icon-picker.js
+++ sdkjs-plugins/content/icons/src/scripts/components/icon-picker.js
@@ -1,122 +1,123 @@
 class IconPicker {
-  #container;
-  #onSelectIconCallback = () => {};
-  #listOfIconNames;
-  #selectedIcons;
-  #clearSelectionButton;
+    #container;
+    #onSelectIconCallback = () => {};
+    #listOfIconNames;
+    #selectedIcons;
+    #clearSelectionButton;
 
-  constructor(catalogOfIcons, containerId) {
-    this.#container = document.getElementById(containerId);
-    this.#clearSelectionButton = document.getElementById("clear");
-    this.#addEventListener();
-    this.show(catalogOfIcons);
-  }
+    constructor(catalogOfIcons) {
+        this.#container = document.getElementById("icons");
+        this.#clearSelectionButton = document.getElementById("clear");
+        this.#addEventListener();
+        this.show(catalogOfIcons);
+    }
 
-  show(catalogOfIcons, categoryId = "") {
-    this.#listOfIconNames = new Set();
-    this.#selectedIcons = new Map();
-    this.#container.textContent = "";
-    const fragment = document.createDocumentFragment();
+    show(catalogOfIcons, categoryId = "") {
+        this.#listOfIconNames = new Set();
+        this.#selectedIcons = new Map();
+        this.#container.textContent = "";
+        const fragment = document.createDocumentFragment();
 
-    catalogOfIcons.forEach((categoryInfo) => {
-      let id = categoryInfo.id;
-      let icons = categoryInfo.icons;
-      if (categoryId !== "" && categoryId !== id) {
-        return;
-      }
+        catalogOfIcons.forEach((categoryInfo) => {
+            let id = categoryInfo.id;
+            let icons = categoryInfo.icons;
+            if (categoryId !== "" && categoryId !== id) {
+                return;
+            }
 
-      icons.forEach((icon) => {
-        if (this.#listOfIconNames.has(icon.name)) {
-          return;
-        }
-        this.#listOfIconNames.add(icon.name);
-        let img = this.#createIcon(icon.name, icon.folder);
-        fragment.appendChild(img);
-      });
-      this.#onChange();
-    });
+            icons.forEach((icon) => {
+                if (this.#listOfIconNames.has(icon.name)) {
+                    return;
+                }
+                this.#listOfIconNames.add(icon.name);
+                let img = this.#createIcon(icon.name, icon.folder);
+                fragment.appendChild(img);
+            });
+            this.#onChange();
+        });
 
-    this.#container.appendChild(fragment);
-  }
+        this.#container.appendChild(fragment);
+    }
 
-  setOnSelectIconCallback(callback) {
-    this.#onSelectIconCallback = callback;
-  }
+    setOnSelectIconCallback(callback) {
+        this.#onSelectIconCallback = callback;
+    }
 
-  #addEventListener() {
-    this.#container.addEventListener("click", (e) => {
-      const icon = e.target.closest(".icon");
-      if (icon) {
-        let iconId = icon.getAttribute("data-name");
-        let section = icon.getAttribute("data-section");
-        if (this.#selectedIcons.has(iconId)) {
-          icon.classList.remove("selected");
-          this.#selectedIcons.delete(iconId);
-        } else {
-          icon.classList.add("selected");
-          this.#selectedIcons.set(iconId, section);
-        }
+    #addEventListener() {
+        this.#container.addEventListener("click", (e) => {
+            const icon = e.target.closest(".icon");
+            if (icon) {
+                let iconId = icon.getAttribute("data-name");
+                let section = icon.getAttribute("data-section");
+                if (this.#selectedIcons.has(iconId)) {
+                    icon.classList.remove("selected");
+                    this.#selectedIcons.delete(iconId);
+                } else {
+                    icon.classList.add("selected");
+                    this.#selectedIcons.set(iconId, section);
+                }
+                this.#onChange();
+            }
+        });
+        this.#container.addEventListener("dblclick", (e) => {
+            const icon = e.target.closest(".icon");
+            if (icon) {
+                let iconId = icon.getAttribute("data-name");
+                let section = icon.getAttribute("data-section");
+                icon.classList.add("selected");
+                this.#selectedIcons.set(iconId, section);
+                const needToRun = true;
+                this.#onSelectIconCallback(this.#selectedIcons, needToRun);
+            }
+        });
+        this.#clearSelectionButton.addEventListener(
+            "click",
+            this.#unselectAll.bind(this)
+        );
+    }
+
+    #unselectAll() {
+        this.#selectedIcons = new Map();
+        this.#container.querySelectorAll(".icon.selected").forEach((icon) => {
+            icon.classList.remove("selected");
+        });
         this.#onChange();
-      }
-    });
-    this.#container.addEventListener("dblclick", (e) => {
-      const icon = e.target.closest(".icon");
-      if (icon) {
-        let iconId = icon.getAttribute("data-name");
-        let section = icon.getAttribute("data-section");
-        icon.classList.add("selected");
-        this.#selectedIcons.set(iconId, section);
-        const needToRun = true;
-        this.#onSelectIconCallback(this.#selectedIcons, needToRun);
-      }
-    });
-    this.#clearSelectionButton.addEventListener(
-      "click",
-      this.#unselectAll.bind(this)
-    );
-  }
+    }
 
-  #unselectAll() {
-    this.#selectedIcons = new Map();
-    this.#container.querySelectorAll(".icon.selected").forEach((icon) => {
-      icon.classList.remove("selected");
-    });
-    this.#onChange();
-  }
+    #onChange() {
+        const total = this.#listOfIconNames.size;
+        const selected =
+            this.#container.querySelectorAll(".icon.selected").length;
+        document.getElementById(
+            "total"
+        ).textContent = `${total} icons, ${selected} selected`;
 
-  #onChange() {
-    const total = this.#listOfIconNames.size;
-    const selected = this.#container.querySelectorAll(".icon.selected").length;
-    document.getElementById(
-      "total"
-    ).textContent = `${total} icons, ${selected} selected`;
+        this.#onSelectIconCallback(this.#selectedIcons);
+    }
 
-    this.#onSelectIconCallback(this.#selectedIcons);
-  }
+    #createIcon(iconId, section) {
+        const svgNS = "http://www.w3.org/2000/svg";
+        const xlinkNS = "http://www.w3.org/1999/xlink";
 
-  #createIcon(iconId, section) {
-    const svgNS = "http://www.w3.org/2000/svg";
-    const xlinkNS = "http://www.w3.org/1999/xlink";
+        const fragment = document.createDocumentFragment();
+        const svg = document.createElementNS(svgNS, "svg");
+        fragment.appendChild(svg);
+        svg.setAttribute("class", "icon");
+        svg.setAttribute("role", "img");
+        svg.setAttribute("data-name", iconId);
+        svg.setAttribute("data-section", section);
 
-    const fragment = document.createDocumentFragment();
-    const svg = document.createElementNS(svgNS, "svg");
-    fragment.appendChild(svg);
-    svg.setAttribute("class", "icon");
-    svg.setAttribute("role", "img");
-    svg.setAttribute("data-name", iconId);
-    svg.setAttribute("data-section", section);
+        const title = document.createElementNS(svgNS, "title");
+        svg.appendChild(title);
+        title.textContent = iconId;
 
-    const title = document.createElementNS(svgNS, "title");
-    svg.appendChild(title);
-    title.textContent = iconId;
+        const use = document.createElementNS(svgNS, "use");
+        svg.appendChild(use);
+        use.setAttributeNS(xlinkNS, "xlink:href", `#${iconId}`);
+        use.setAttribute("href", `#${iconId}`);
 
-    const use = document.createElementNS(svgNS, "use");
-    svg.appendChild(use);
-    use.setAttributeNS(xlinkNS, "xlink:href", `#${iconId}`);
-    use.setAttribute("href", `#${iconId}`);
-
-    return fragment;
-  }
+        return fragment;
+    }
 }
 
 export { IconPicker };
