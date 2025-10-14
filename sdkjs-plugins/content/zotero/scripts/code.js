465b277c6f8079f1f439a9d97c4bcf0ca28d7297
Artur <artur.drapeza@onlyoffice.com>
2025-10-14 17:23:56 +0300

    Display citation as (for notes) -> radiobuttons

--- sdkjs-plugins/content/zotero/scripts/code.js
+++ sdkjs-plugins/content/zotero/scripts/code.js
@@ -104,7 +104,7 @@
         styleSelect: document.getElementById("styleSelect"),
         styleLang: document.getElementById("styleLang"),
         notesStyleWrapper: document.querySelector(".notesStyle"),
-        notesStyleInput: document.querySelector(".notesStyle input"),
+        notesStyleRadios: document.querySelectorAll(".notesStyle input[name=notesAs]"),
 
         insertBibBtn: document.getElementById("insertBibBtn"),
         insertLinkBtn: document.getElementById("insertLinkBtn"),
@@ -446,12 +446,15 @@
             selectedLocale = val;
         };
 
-        elements.notesStyleInput.onselectchange = function (inp, val, isClick) {
-            if (!isClick) {
-                return;
-            }
-            cslStylesManager.saveLastUsedNotesStyle(val);
-        }
+        elements.notesStyleRadios.forEach(radio => {
+            radio.addEventListener('change', function(event)  {
+                if (event.target.checked) {
+                    cslStylesManager.saveLastUsedNotesStyle(event.target.value);
+                }
+            });
+        });
+            
+        
     }
 
     var scrollBoxes = [];
@@ -609,7 +612,7 @@
 
         let notesStyle = cslStylesManager.getLastUsedNotesStyle();
         citationDocService.setNotesStyle(notesStyle);
-        elements.notesStyleInput.value = notesStyle;
+        elements.notesStyleWrapper.querySelector('input[name="notesAs"][value="' + notesStyle + '"]').checked = true;
     }
 
     function applyTranslations() {
