84ba0008f516f10a21015c6c67bc5a701105062e
Artur <artur.drapeza@onlyoffice.com>
2025-10-31 12:22:21 +0300

    Ability to use incomplete styles that have a parent

--- sdkjs-plugins/content/zotero/scripts/csl/styles/style-parser.js
+++ sdkjs-plugins/content/zotero/scripts/csl/styles/style-parser.js
@@ -4,6 +4,7 @@
  * @typedef {Object} StyleInfo
  * @property {{fields: Array<string>, format: string}} categories - An object containing the citation format and fields.
  * @property {number} dependent - A dependent style is one that requires a specific style to be installed.
+ * @property {string} [parent] - The URL of the parent style.
  * @property {string} href - The URL of the style.
  * @property {string} name - The name of the style.
  * @property {string} title - The title of the style.
@@ -45,6 +46,15 @@ const CslStylesParser = {
         if (href) {
             let attribute = href.getAttribute("href");
             if (attribute) styleInfo.href = attribute;
+        }
+
+        const parent = xmlDoc.querySelector(
+            'info link[rel="independent-parent"]'
+        );
+        if (parent) {
+            let attribute = parent.getAttribute("href");
+            if (attribute) styleInfo.parent = attribute;
+            styleInfo.dependent = 1;
         }
 
         const updated = xmlDoc.querySelector("info updated");
