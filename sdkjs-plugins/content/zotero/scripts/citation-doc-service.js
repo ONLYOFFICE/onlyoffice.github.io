39417a30f74380e7c9c620db4716639485a330dc
Artur <artur.drapeza@onlyoffice.com>
2025-11-04 18:46:30 +0300

    Bug 66380 - Superscript and subscript formatting is missing when inserting a bibliography using the Zotero plugin.

--- sdkjs-plugins/content/zotero/scripts/citation-doc-service.js
+++ sdkjs-plugins/content/zotero/scripts/citation-doc-service.js
@@ -8,6 +8,15 @@
  */
 
 /**
+ * @typedef {Object} SupSubPositions
+ * @property {'sup'|'sub'} type
+ * @property {number} start
+ * @property {number} end
+ * @property {string} content
+ * @property {string} originalMatch
+ */
+
+/**
  * @param {string} citPrefix
  * @param {string} citSuffix
  * @param {string} bibPrefix
@@ -42,17 +51,32 @@ function CitationDocService(
 }
 
 /**
+ * @param {CustomField} field
+ * @returns {Promise<void>}
+ */
+CitationDocService.prototype._addAddinField = function (field) {
+    return new Promise(function (resolve) {
+        window.Asc.plugin.executeMethod("AddAddinField", [field], resolve);
+    });
+};
+
+/**
  * @param {string} text
  * @param {string} value
+ * @returns {Promise<void>}
  */
 CitationDocService.prototype.addBibliography = function (text, value) {
+    const self = this;
+    const supSubPositions = this._removeSuperSubTagsWithPositions(text);
     /** @type {CustomField} */
     const field = {
         Value: this._bibPrefix + value + this._bibSuffix,
-        Content: text,
+        Content: supSubPositions.text,
     };
-    return new Promise(function (resolve) {
-        window.Asc.plugin.executeMethod("AddAddinField", [field], resolve);
+
+    return this._addAddinField(field).then(function () {
+        if (!supSubPositions.positions.length) return;
+        return self._setSuperSubByPositions(supSubPositions.positions);
     });
 };
 
@@ -62,10 +86,12 @@ CitationDocService.prototype.addBibliography = function (text, value) {
  * @returns
  */
 CitationDocService.prototype.addCitation = function (text, value) {
+    const self = this;
+    const supSubPositions = this._removeSuperSubTagsWithPositions(text);
     /** @type {CustomField} */
     const field = {
         Value: this._citPrefix + " " + this._citSuffix + value,
-        Content: text,
+        Content: supSubPositions.text,
     };
     if ("note" === this._styleFormat) {
         switch (this._notesStyle) {
@@ -83,11 +109,26 @@ CitationDocService.prototype.addCitation = function (text, value) {
                 break;
         }
     }
-    return new Promise(function (resolve, reject) {
-        window.Asc.plugin.executeMethod("AddAddinField", [field], resolve);
+    return this._addAddinField(field).then(function () {
+        if (!supSubPositions.positions.length) return;
+        return self._setSuperSubByPositions(supSubPositions.positions);
     });
 };
-
+/**
+        window.Asc.plugin.executeMethod("AddAddinField", [field], function () {
+            const isCalc = true;
+            const isClose = false;
+            Asc.plugin.callCommand(
+                function () {
+                    const doc = Api.GetDocument();
+                    doc.RemoveSelection();
+                },
+                isClose,
+                isCalc,
+                resolve
+            );
+        });
+*/
 /**
  * @returns {Promise<Array<CustomField>>}
  */
@@ -95,13 +136,16 @@ CitationDocService.prototype.getAllAddinFields = function () {
     const self = this;
     return new Promise(function (resolve, reject) {
         window.Asc.plugin.executeMethod("GetAllAddinFields", null, resolve);
+    }).then(function (arrFields) {
+        console.warn("getAllAddinFields", arrFields);
+        return arrFields;
     });
 };
 
 /**
  * @returns {Promise<Array<CustomField>>}
  */
-CitationDocService.prototype.getAddinZoteroFields = function () {
+CitationDocService.prototype._getAddinZoteroFields = function () {
     const self = this;
     return new Promise(function (resolve, reject) {
         self.getAllAddinFields().then(function (arrFields) {
@@ -125,11 +169,79 @@ CitationDocService.prototype.getAddinZoteroFields = function () {
 };
 
 /**
+ * @param {string} escapedHtmlText
+ * @returns {{text: string, positions: Array<SupSubPositions>}}
+ */
+CitationDocService.prototype._removeSuperSubTagsWithPositions = function (
+    escapedHtmlText
+) {
+    /** @type {Array<SupSubPositions>} */
+    const positions = [];
+    let currentIndex = 0;
+    let result = "";
+
+    let tempResult = escapedHtmlText;
+
+    // Process <sup> tags
+    tempResult = tempResult.replace(
+        /<sup\b[^>]*>([^<]*)<\/sup>/gi,
+        function (match, content, offset) {
+            const start = offset + currentIndex;
+            const end = start + content.length;
+
+            positions.push({
+                type: "sup",
+                content: content,
+                start: start,
+                end: end,
+                originalMatch: match,
+            });
+
+            currentIndex += content.length - match.length;
+            return content;
+        }
+    );
+
+    // Reset indexes for <sub> tags
+    currentIndex = 0;
+    result = tempResult;
+
+    // Process <sub> tags
+    tempResult = tempResult.replace(
+        /<sub\b[^>]*>([^<]*)<\/sub>/gi,
+        function (match, content, offset) {
+            const start = offset + currentIndex;
+            const end = start + content.length;
+
+            positions.push({
+                type: "sub",
+                content: content,
+                start: start,
+                end: end,
+                originalMatch: match,
+            });
+
+            currentIndex += content.length - match.length;
+            return content;
+        }
+    );
+
+    result = tempResult;
+
+    return {
+        text: result,
+        positions: positions.sort(function (a, b) {
+            return a.start - b.start;
+        }),
+    };
+};
+
+/**
  * @returns {Promise<boolean>}
  */
 CitationDocService.prototype.saveAsText = function () {
     // TODO потом добавить ещё форматы, пока только как текст
-    return this.getAddinZoteroFields().then(function (arrFields) {
+    return this._getAddinZoteroFields().then(function (arrFields) {
         let count = arrFields.length;
         if (!count) {
             window.Asc.plugin.executeCommand("close", "");
@@ -168,8 +280,54 @@ CitationDocService.prototype.setStyleFormat = function (styleFormat) {
     this._styleFormat = styleFormat;
 };
 
-CitationDocService.prototype.updateAddinFields = function (fields) {
-    return new Promise(function (resolve, reject) {
-        window.Asc.plugin.executeMethod("UpdateAddinFields", [fields], resolve);
+/**
+ * @param {Array<SupSubPositions>} positions
+ * @returns {Promise<void>}
+ */
+CitationDocService.prototype._setSuperSubByPositions = function (positions) {
+    return new Promise(function (resolve) {
+        const isCalc = true;
+        const isClose = false;
+        Asc.scope.positions = positions;
+        Asc.plugin.callCommand(
+            function () {
+                const doc = Api.GetDocument();
+                let run = doc.GetCurrentRun();
+                Asc.scope.positions.forEach(function (
+                    /** @type {SupSubPositions} */ pos
+                ) {
+                    let range = run.GetRange(pos.start, pos.end);
+                    if ("sup" === pos.type) {
+                        range.SetVertAlign("superscript");
+                    } else {
+                        range.SetVertAlign("subscript");
+                    }
+                });
+            },
+            isClose,
+            isCalc,
+            resolve
+        );
     });
+};
+
+/**
+ * @param {Array<CustomField>} fields
+ * @returns {Promise<void>}
+ */
+CitationDocService.prototype.updateAddinFields = function (fields) {
+    const self = this;
+    /*fields.forEach(function (field) {
+        const supSubPositions = self._removeSuperSubTagsWithPositions(
+            field.Content
+        );
+        field.Content = supSubPositions.text;
+    });*/
+    console.log("updateAddinFields", fields);
+    return new Promise(function (resolve) {
+        window.Asc.plugin.executeMethod("UpdateAddinFields", [fields], resolve);
+    }) /*.then(function () {
+        if (!supSubPositions.positions.length) return;
+        return self._setSuperSubByPositions(supSubPositions.positions);
+    })*/;
 };
