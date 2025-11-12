343112d4146d0e701e6f5cfdc24767abb235f754
Artur <artur.drapeza@onlyoffice.com>
2025-11-12 17:14:50 +0300

    Refactor input styles and enhance SearchInput functionality

--- sdkjs-plugins/content/icons/src/js/components/input/search-input.js
+++ sdkjs-plugins/content/icons/src/js/components/input/search-input.js
@@ -12,7 +12,7 @@ class SearchInput extends InputField {
     constructor(container, options = {}) {
         super(container, {
             type: "search",
-            showClear: true,
+            showClear: false,
             showSearchIcon: true,
             ...options,
         });
@@ -25,15 +25,14 @@ class SearchInput extends InputField {
         if (this._options.showSearchIcon) {
             const icon = document.createElement("span");
             icon.className = "input-field-search-icon";
-            icon.innerHTML = "🔍";
-            icon.style.position = "absolute";
-            icon.style.left = "12px";
-            icon.style.top = "50%";
-            icon.style.transform = "translateY(-50%)";
-            icon.style.color = "#c0c4cc";
-
+            icon.innerHTML =
+                '<svg width="14" height="14" viewBox="0 0 14 14" ' +
+                'fill="none" xmlns="http://www.w3.org/2000/svg">' +
+                '<path fill-rule="evenodd" clip-rule="evenodd" ' +
+                'd="M10 5.5C10 7.98528 7.98528 10 5.5 10C3.01472 10 1 7.98528 1 5.5C1 3.01472 3.01472 1 5.5 1C7.98528 1 10 3.01472 10 5.5ZM9.01953 9.72663C8.06578 10.5217 6.83875 11 5.5 11C2.46243 11 0 8.53757 0 5.5C0 2.46243 2.46243 0 5.5 0C8.53757 0 11 2.46243 11 5.5C11 6.83875 10.5217 8.06578 9.72663 9.01953L13.8536 13.1465L13.1465 13.8536L9.01953 9.72663Z" ' +
+                'fill="black" fill-opacity="0.8"/>' +
+                "</svg>";
             this._container.querySelector(".input-field-main")?.prepend(icon);
-            this.input.style.paddingLeft = "36px";
         }
     }
 }
