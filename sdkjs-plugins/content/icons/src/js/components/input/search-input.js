e0c57dec2349e975ce9f50832d553295c49d4681
Artur <artur.drapeza@onlyoffice.com>
2025-11-14 12:23:16 +0300

    Enhance input components: update color variables for better theming support and improve SVG icon fill handling

--- sdkjs-plugins/content/icons/src/js/components/input/search-input.js
+++ sdkjs-plugins/content/icons/src/js/components/input/search-input.js
@@ -34,7 +34,7 @@ class SearchInput extends InputField {
                 'fill="none" xmlns="http://www.w3.org/2000/svg">' +
                 '<path fill-rule="evenodd" clip-rule="evenodd" ' +
                 'd="M10 5.5C10 7.98528 7.98528 10 5.5 10C3.01472 10 1 7.98528 1 5.5C1 3.01472 3.01472 1 5.5 1C7.98528 1 10 3.01472 10 5.5ZM9.01953 9.72663C8.06578 10.5217 6.83875 11 5.5 11C2.46243 11 0 8.53757 0 5.5C0 2.46243 2.46243 0 5.5 0C8.53757 0 11 2.46243 11 5.5C11 6.83875 10.5217 8.06578 9.72663 9.01953L13.8536 13.1465L13.1465 13.8536L9.01953 9.72663Z" ' +
-                'fill="black" fill-opacity="0.8"/>' +
+                'fill="currentColor"/>' +
                 "</svg>";
             this._container
                 .querySelector(".input-field-main")
