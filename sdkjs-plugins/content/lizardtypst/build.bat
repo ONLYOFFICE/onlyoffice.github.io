639a81b367b3806e98443e8f99b52565bbbaabf7
jijinbei <hallucigenia2002@gmail.com>
2025-12-12 17:08:05 +0900

    Modifications to the build script for ziping CHANGELOG.md

--- sdkjs-plugins/content/lizardtypst/build.bat
+++ sdkjs-plugins/content/lizardtypst/build.bat
@@ -3,7 +3,7 @@ set PLUGIN_NAME=lizardtypst
 echo Building %PLUGIN_NAME%.plugin...
 if not exist deploy mkdir deploy
 if exist deploy\%PLUGIN_NAME%.plugin del deploy\%PLUGIN_NAME%.plugin
-powershell -Command "Compress-Archive -Path config.json,index.html,README.md,scripts,resources -DestinationPath deploy\%PLUGIN_NAME%.zip -Force"
+powershell -Command "Compress-Archive -Path config.json,index.html,README.md,scripts,resources,CHANGELOG.md -DestinationPath deploy\%PLUGIN_NAME%.zip -Force"
 move /Y deploy\%PLUGIN_NAME%.zip deploy\%PLUGIN_NAME%.plugin >nul
 echo Done! Created deploy\%PLUGIN_NAME%.plugin
 dir deploy\%PLUGIN_NAME%.plugin | find "%PLUGIN_NAME%"
