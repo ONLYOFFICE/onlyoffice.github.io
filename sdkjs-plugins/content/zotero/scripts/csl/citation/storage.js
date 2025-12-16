6f0c4e0adbcceeae5da9b1f6cfbbf733b94513d2
Artur <artur.drapeza@onlyoffice.com>
2025-12-16 21:34:09 +0300

    license text

--- sdkjs-plugins/content/zotero/scripts/csl/citation/storage.js
+++ sdkjs-plugins/content/zotero/scripts/csl/citation/storage.js
@@ -1,3 +1,35 @@
+/*
+ * (c) Copyright Ascensio System SIA 2010-2025
+ *
+ * This program is a free software product. You can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License (AGPL)
+ * version 3 as published by the Free Software Foundation. In accordance with
+ * Section 7(a) of the GNU AGPL its Section 15 shall be amended to the effect
+ * that Ascensio System SIA expressly excludes the warranty of non-infringement
+ * of any third-party rights.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
+ * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+ *
+ * You can contact Ascensio System SIA at 20A-6 Ernesta Birznieka-Upish
+ * street, Riga, Latvia, EU, LV-1050.
+ *
+ * The  interactive user interfaces in modified source and object code versions
+ * of the Program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU AGPL version 3.
+ *
+ * Pursuant to Section 7(b) of the License you must retain the original Product
+ * logo when distributing the program. Pursuant to Section 7(e) we decline to
+ * grant you any rights under trademark law for use of our trademarks.
+ *
+ * All the Product's GUI elements, including illustrations and icon sets, as
+ * well as technical writing content are licensed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International. See the License
+ * terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ *
+ */
+
 // @ts-check
 /// <reference path="./citation-item.js" />
 
@@ -19,8 +51,8 @@ const CSLCitationStorage = {
         return null;
     },
     /**
-     * 
-     * @param {string|number} id 
+     *
+     * @param {string|number} id
      * @returns {number}
      */
     getIndex: function (id) {
@@ -34,7 +66,7 @@ const CSLCitationStorage = {
         return this;
     },
     /**
-     * @param {string|number} id 
+     * @param {string|number} id
      * @returns {CSLCitationStorage}
      */
     delete: function (id) {
@@ -48,7 +80,7 @@ const CSLCitationStorage = {
         return this;
     },
     /**
-     * @param {function(CitationItem, string, CSLCitationStorage?)} callback 
+     * @param {function(CitationItem, string, CSLCitationStorage?)} callback
      */
     forEach: function (callback) {
         for (var i = 0; i < this.size; i++) {
@@ -56,7 +88,7 @@ const CSLCitationStorage = {
         }
     },
     /**
-     * @param {string|number} id 
+     * @param {string|number} id
      * @returns {boolean}
      */
     has: function (id) {
@@ -64,8 +96,8 @@ const CSLCitationStorage = {
         return this._ids.indexOf(id) >= 0;
     },
     /**
-     * @param {string|number} id 
-     * @param {CitationItem} item 
+     * @param {string|number} id
+     * @param {CitationItem} item
      * @returns {CSLCitationStorage}
      */
     set: function (id, item) {
