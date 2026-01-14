8c68eed9e63f5cc225f303f0120940725a7772ab
Artur <artur.drapeza@onlyoffice.com>
2026-01-14 19:50:25 +0300

    Add Custom Assistant window

--- sdkjs-plugins/content/ai/scripts/customAssistant.js
+++ sdkjs-plugins/content/ai/scripts/customAssistant.js
@@ -30,221 +30,73 @@
  *
  */
 
-(function (window, undefined) {
-    let errTimeout = null;
-    let loader = null;
-    let themeType = "light";
-    let regenerationMessageIndex = null; //Index of the message for which a new reply is being created
+// @ts-check
 
-    let scrollbarList;
+/// <reference path="../../../v1/plugins.js" />
+/// <reference path="./utils/theme.js" />
 
-    let localStorageKey = "onlyoffice_ai_chat_state";
+(function (window) {
+    const mainContainer = document.getElementById("custom_assistant_window");
+
+    const tempTextarea = document.getElementById("input_prompt");
+    if (tempTextarea instanceof HTMLTextAreaElement === false) {
+        console.error("Custom Assistant: textarea is not HTMLTextAreaElement");
+        return;
+    }
+    /** @type {HTMLTextAreaElement} */
+    const textarea = tempTextarea;
+
+    if (
+        !textarea ||
+        !mainContainer ||
+        textarea instanceof HTMLTextAreaElement === false
+    ) {
+        console.error("Custom Assistant: required elements are missing");
+        return;
+    }
 
     window.Asc.plugin.init = function () {
-        scrollbarList = new PerfectScrollbar("#custom_assistant", {});
-        restoreState();
-
-        updateTextareaSize();
-
         window.Asc.plugin.sendToPlugin("onWindowReady", {});
 
-        document
-            .getElementById("input_prompt_submit")
-            .addEventListener("click", function () {
-                onSubmit();
-            });
-        document.getElementById("input_prompt").onkeydown = function (e) {
-            if ((e.ctrlKey || e.metaKey) && e.key === "Enter") {
-                e.target.value += "\n";
-                updateTextareaSize();
-            } else if (e.key === "Enter") {
+        textarea.focus();
+
+        mainContainer.addEventListener("click", function (e) {
+            if (e.target instanceof HTMLAnchorElement) {
                 e.preventDefault();
-                e.stopPropagation();
-                onSubmit();
+                window.open(e.target.href, "_blank");
             }
-        };
-
-        document
-            .getElementById("input_prompt")
-            .addEventListener("focus", function (event) {
-                document
-                    .getElementById("input_prompt_wrapper")
-                    .classList.add("focused");
-            });
-        document
-            .getElementById("input_prompt")
-            .addEventListener("blur", function (event) {
-                document
-                    .getElementById("input_prompt_wrapper")
-                    .classList.remove("focused");
-            });
-        document.getElementById("input_prompt").focus();
-
-        document
-            .getElementById("input_prompt")
-            .addEventListener("input", function (event) {
-                //autosize
-                updateTextareaSize();
-            });
-
-        document
-            .getElementById("custom_assistant_wrapper")
-            .addEventListener("click", function (e) {
-                if (e.target.tagName === "A") {
-                    e.preventDefault();
-                    window.open(e.target.href, "_blank");
-                }
-            });
+        });
     };
-
-    function onSubmit() {
-        let textarea = document.getElementById("input_prompt");
-
-        let value = textarea.value.trim();
-        if (value.length) {
-            sendMessage(textarea.value.trim());
-            textarea.value = "";
-            updateTextareaSize();
-            document.getElementById("cur_tokens").innerText = 0;
-        }
-    }
-
-    function updateStartPanel() {
-        updateWelcomeText();
-    }
-
-    function updateWelcomeText() {
-        let welcomeText = window.Asc.plugin.tr("Welcome");
-        if (window.Asc.plugin.info.userName) {
-            welcomeText += ", " + window.Asc.plugin.info.userName;
-        }
-        welcomeText += "!";
-        document
-            .getElementById("welcome_text")
-            .prepend("<span>" + welcomeText + "</span>");
-    }
-
-    function updateTextareaSize() {
-        let textarea = document.getElementById("input_prompt");
-        if (textarea) {
-            textarea.style.height = "auto";
-            textarea.style.height =
-                Math.min(textarea.scrollHeight, 98) + 2 + "px";
-        }
-    }
-
-    function setState(state) {
-        window.localStorage.setItem(localStorageKey, JSON.stringify(state));
-    }
-
-    function getState() {
-        let state = window.localStorage.getItem(localStorageKey);
-        return state ? JSON.parse(state) : null;
-    }
-
-    function restoreState() {
-        let state = getState();
-        if (!state) return;
-
-        if (state.messages) {
-            messagesList.set(state.messages);
-        }
-        if (state.inputValue) {
-            document.getElementById("input_prompt").value = state.inputValue;
-        }
-    }
-
-    function sendMessage(text) {
-        const isRegenerating = regenerationMessageIndex !== null;
-        const message = { role: "user", content: text };
-
-        if (!isRegenerating) {
-            messagesList.add(message);
-            createTyping();
-        }
-
-        let list = isRegenerating
-            ? messagesList.get().slice(0, regenerationMessageIndex)
-            : messagesList.get();
-
-        //Remove the errors and user messages that caused the error
-        list = list.filter(function (item, index) {
-            const nextItem = list[index + 1];
-            return !item.error && !(nextItem && nextItem.error);
-        });
-        list = list.map(function (item) {
-            return { role: item.role, content: item.getActiveContent() };
-        });
-
-        window.Asc.plugin.sendToPlugin("onChatMessage", list);
-    }
-
-    function createTyping() {
-        let chatEl = document.getElementById("custom_assistant");
-        let messageEl = document.createElement("div");
-        messageEl.id = "loading";
-        messageEl.classList.add("message");
-        messageEl.style.order = messagesList.get().length;
-
-        let spanMessageEl = document.createElement("div");
-        spanMessageEl.classList.add("span_message");
-        spanMessageEl.textContent = window.Asc.plugin.tr("Thinking");
-        messageEl.append(spanMessageEl);
-        chatEl.prepend(messageEl);
-        chatEl.scrollTop(chatEl.scrollHeight);
-        interval = setInterval(function () {
-            let countDots = (spanMessageEl.text().match(/\./g) || []).length;
-            countDots = countDots < 3 ? countDots + 1 : 0;
-            spanMessageEl.text(
-                window.Asc.plugin.tr("Thinking") +
-                    Array(countDots + 1).join(".")
-            );
-        }, 500);
-    }
-
-    function onResize() {
-        updateTextareaSize();
-        scrollbarList && scrollbarList.update();
-    }
 
     function onThemeChanged(theme) {
         window.Asc.plugin.onThemeChangedBase(theme);
-
-        themeType = theme.type || "light";
         updateBodyThemeClasses(theme.type, theme.name);
         updateThemeVariables(theme);
     }
-
-    window.addEventListener("resize", onResize);
-    onResize();
 
     window.Asc.plugin.onTranslate = function () {
         let elements = document.querySelectorAll(".i18n");
 
         elements.forEach(function (element) {
+            if (element instanceof HTMLElement === false) {
+                return;
+            }
             element.innerText = window.Asc.plugin.tr(element.innerText);
         });
 
         // Textarea
-        document
-            .getElementById("input_prompt")
-            .setAttribute(
-                "placeholder",
-                window.Asc.plugin.tr("Enter your query here...")
-            );
-
-        updateStartPanel();
+        textarea.setAttribute(
+            "placeholder",
+            window.Asc.plugin.tr("Enter your query here...")
+        );
     };
 
     window.Asc.plugin.onThemeChanged = onThemeChanged;
-
     window.Asc.plugin.attachEvent("onThemeChanged", onThemeChanged);
 
-    window.Asc.plugin.attachEvent("onUpdateState", function () {
-        setState({
-            inputValue: document.getElementById("input_prompt").value,
-            type: "", // paint, replace, underline
-        });
-        window.Asc.plugin.sendToPlugin("onUpdateState");
+    window.Asc.plugin.attachEvent("onClickAdd", () => {
+        let value = textarea.value.trim();
+        textarea.focus();
+        window.Asc.plugin.sendToPlugin("onAddNewAssistant", value);
     });
-})(window, undefined);
+})(window);
