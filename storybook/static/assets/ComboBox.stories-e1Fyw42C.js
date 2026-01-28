a5b20734059dd54fe6535d58a4cecb9df2b2803f
Alexander Trofimov <alexander.trofimov@onlyoffice.com>
2026-01-28 12:22:39 +0300

    Bump storybook from 9.0.6 to 9.1.17

--- storybook/static/assets/ComboBox.stories-e1Fyw42C.js
+++ storybook/static/assets/ComboBox.stories-e1Fyw42C.js
@@ -1,15 +1,15 @@
-const a={title:"Components/ComboBox",tags:["autodocs"],parameters:{docs:{description:{component:"ComboBox from ONLYOFFICE plugin UI."}}}},o=()=>`
+const e={title:"Components/ComboBox",tags:["autodocs"],parameters:{docs:{description:{component:"ComboBox from ONLYOFFICE plugin UI."}}}},o=()=>`
   <label class="header" for="select_example">ComboBox</label>
   <select id="select_example" class="">
     <option value="1">Item 1</option>
     <option value="2">Item 2</option>
     <option value="3">Item 3</option>
   </select>
-`;o.storyName="ComboBox";var e,t,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`() => \`
+`;o.storyName="ComboBox";o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`() => \`
   <label class="header" for="select_example">ComboBox</label>
   <select id="select_example" class="">
     <option value="1">Item 1</option>
     <option value="2">Item 2</option>
     <option value="3">Item 3</option>
   </select>
-\``,...(s=(t=o.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const l=["ComboBox"];export{o as ComboBox,l as __namedExportsOrder,a as default};
+\``,...o.parameters?.docs?.source}}};const t=["ComboBox"];export{o as ComboBox,t as __namedExportsOrder,e as default};
