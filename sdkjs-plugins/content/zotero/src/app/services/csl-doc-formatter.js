75a504f231497a229f7e5bb4f1abcd3bc98f1186
Artur <artur.drapeza@onlyoffice.com>
2026-03-04 16:15:56 +0300

    Vancouver Citation Issue (numeric style with superscript)

--- sdkjs-plugins/content/zotero/src/app/services/csl-doc-formatter.js
+++ sdkjs-plugins/content/zotero/src/app/services/csl-doc-formatter.js
@@ -62,27 +62,44 @@ class CslDocFormatter {
                     if (!selRange) {
                         return;
                     }
+                    /**
+                     * @param {ApiRange} range 
+                     * @param {string} type 
+                     */
+                    function applyFormatting(range, type) {
+                        if ("sup" === type) {
+                            range.SetVertAlign("superscript");
+                        } else if ("sub" === type) {
+                            range.SetVertAlign("subscript");
+                        } else if ("sc" === type) {
+                            range.SetSmallCaps(true);
+                        } else if ("u" === type) {
+                            range.SetUnderline(true);
+                        } else if ("b" === type) {
+                            range.SetBold(true);
+                        } else if ("i" === type || "em" === type) {
+                            range.SetItalic(true);
+                        }
+                    }
+
+                    if (Asc.scope.formatting.length === 1) {
+                        const pos = Asc.scope.formatting[0];
+                        if (pos.start === 0 && pos.end === selRange.GetText().length) {
+                            applyFormatting(selRange, pos.type);
+                            return;
+                        }
+                    }
+                    
                     doc.MoveCursorToPos(
                         selRange.GetEndPos() - Asc.scope.text.length,
                     );
                     let run = doc.GetCurrentRun();
+
                     for (let i = Asc.scope.formatting.length - 1; i >= 0; i--) {
                         const pos = Asc.scope.formatting[i];
                         let range = run.GetRange(pos.start, pos.end);
                         if (!range) continue;
-                        if ("sup" === pos.type) {
-                            range.SetVertAlign("superscript");
-                        } else if ("sub" === pos.type) {
-                            range.SetVertAlign("subscript");
-                        } else if ("sc" === pos.type) {
-                            range.SetSmallCaps(true);
-                        } else if ("u" === pos.type) {
-                            range.SetUnderline(true);
-                        } else if ("b" === pos.type) {
-                            range.SetBold(true);
-                        } else if ("i" === pos.type || "em" === pos.type) {
-                            range.SetItalic(true);
-                        }
+                        applyFormatting(range, pos.type)
                     }
                 },
                 isClose,
